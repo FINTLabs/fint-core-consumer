08ff558791c979e29c4761725492a524cae7bab3
Hknots <hknots99@gmail.com>
2024-04-18 15:03:37 +0200

    rename

--- src/main/java/no/fintlabs/consumer/Application.java
+++ src/main/java/no/fintlabs/consumer/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FintCoreConsumerApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(FintCoreConsumerApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
