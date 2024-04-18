58de9d62b2a64a52e021a8104473731bd0cb822a
Hknots <hknots99@gmail.com>
2024-04-18 17:32:24 +0200

    move AdminController

---
+++ src/main/java/no/fintlabs/consumer/admin/AdminController.java
@@ -0,0 +1,26 @@
+package no.fintlabs.consumer.admin;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import static no.fintlabs.consumer.config.Endpoints.ADMIN;
+
+@RestController
+@RequestMapping(ADMIN)
+public class AdminController {
+
+    @GetMapping("/health")
+    public ResponseEntity<?> getHealthChecks() {
+        return null;
+    }
+
+    @GetMapping("/cache/status")
+    public Map<String, CacheEntry> getCacheStatus() {
+        return null;
+    }
+
+}
