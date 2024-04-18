5621ea4ba15c3a0f5bd067a5fcbe2561f9412206
Henrik Knutsen <hknut99@gmail.com>
2024-04-18 14:57:59 +0200

    initial commit

---
+++ src/test/java/no/fintlabs/consumer/FintCoreConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package no.fintlabs.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintCoreConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
