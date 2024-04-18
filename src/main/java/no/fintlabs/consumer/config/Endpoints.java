63a4827ed954a31a84a1462e0c84ae158a440062
Hknots <hknots99@gmail.com>
2024-04-18 15:31:34 +0200

    add Endpoints

---
+++ src/main/java/no/fintlabs/consumer/config/Endpoints.java
@@ -0,0 +1,9 @@
+package no.fintlabs.consumer.config;
+
+public class Endpoints {
+
+    public static final String DEFAULT_ENDPOINT = "${fint.consumer.domain}/${fint.consumer.package}";
+    public static final String ADMIN = DEFAULT_ENDPOINT + "/admin";
+
+
+}
