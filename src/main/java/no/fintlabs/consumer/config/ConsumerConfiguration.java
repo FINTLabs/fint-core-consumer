a477996b0912f990bb7074912da544db9f4727f9
Hknots <hknots99@gmail.com>
2024-04-25 12:56:34 +0200

    add env

--- src/main/java/no/fintlabs/consumer/config/ConsumerConfiguration.java
+++ src/main/java/no/fintlabs/consumer/config/ConsumerConfiguration.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ConsumerConfiguration {
 
+    @Value("${fint.consumer.env}")
+    private String env;
+
     @Value("${fint.consumer.domain}")
     private String domain;
 
