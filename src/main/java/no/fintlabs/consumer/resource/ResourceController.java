8ecc1a4413086f6337acf05357967efeb20094a3
Hknots <hknots99@gmail.com>
2024-04-29 13:31:07 +0200

    implement some endpoints and test data

--- src/main/java/no/fintlabs/consumer/resource/ResourceController.java
+++ src/main/java/no/fintlabs/consumer/resource/ResourceController.java
@@ -3,8 +3,13 @@ package no.fintlabs.consumer.resource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.fint.model.FintResource;
-import no.fintlabs.cache.CacheContainer;
+import no.fint.model.felles.kompleksedatatyper.Identifikator;
+import no.fint.model.resource.administrasjon.personal.FravarResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Map;
 
 import static no.fintlabs.consumer.config.Endpoints.*;
 
@@ -14,40 +19,60 @@ import static no.fintlabs.consumer.config.Endpoints.*;
 @Slf4j
 public class ResourceController {
 
-    private final CacheContainer cacheContainer;
+    private final ResourceService resourceService;
+    private final CacheService cacheService;
 
+    // TODO: Make use of HATEOS -> fint-core-relations
     @GetMapping
-    public void getResource(@PathVariable String resource) {
-
+    public Collection<FintResource> getResource(@PathVariable String resource,
+                                                @RequestParam(defaultValue = "0") int size,
+                                                @RequestParam(defaultValue = "0") int offset,
+                                                @RequestParam(defaultValue = "0") long sinceTimeStamp) {
+        return resourceService.getResources(resource, size, offset, sinceTimeStamp);
     }
 
     @GetMapping(BY_ID)
-    public FintResource getResourceById(@PathVariable String resource,
-                                        @PathVariable String idField,
-                                        @PathVariable String idValue) {
-        return null;
+    public ResponseEntity<FintResource> getResourceById(@PathVariable String resource,
+                                                        @PathVariable String idField,
+                                                        @PathVariable String idValue) {
+        return resourceService.getResourceById(resource, idField, idValue)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping(LAST_UPDATED)
-    public void getLastUpdated(@PathVariable String resource) {
-
+    public Map<String, Long> getLastUpdated(@PathVariable String resource) {
+        return Map.of("lastUpdated", cacheService.getCache(resource).getLastUpdated());
     }
 
     @GetMapping(CACHE_SIZE)
-    public void getResourceCacheSize(@PathVariable String resource) {
-
+    public Map<String, Integer> getResourceCacheSize(@PathVariable String resource) {
+        return Map.of("size", cacheService.getCache(resource).size());
     }
+
+    // TODO: Methods under requires Kafka
 
     @PostMapping
     public void postResource(@PathVariable String resource) {
+        FravarResource fravarResource = new FravarResource();
 
+        Identifikator identifikator1 = new Identifikator();
+        identifikator1.setIdentifikatorverdi("321");
+        fravarResource.setKildesystemId(identifikator1);
+
+        Identifikator identifikator = new Identifikator();
+        identifikator.setIdentifikatorverdi("123");
+        fravarResource.setSystemId(identifikator);
+
+
+        log.info("Posting resource with ID: {}", identifikator.getIdentifikatorverdi());
+        cacheService.getCache(resource).put("test", fravarResource, resourceService.hashCodes(fravarResource));
     }
 
     @PutMapping(BY_ID)
     public void putResourceById(@PathVariable String resource,
                                 @PathVariable String idField,
                                 @PathVariable String idValue) {
-
     }
 
 }
