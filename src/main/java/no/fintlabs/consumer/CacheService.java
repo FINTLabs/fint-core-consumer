70114e41373972dc0c03a9a10f8c66e7e488b90a
Hknots <hknots99@gmail.com>
2024-04-29 10:33:04 +0200

    create one cache for each resource

--- src/main/java/no/fintlabs/consumer/CacheService.java
+++ src/main/java/no/fintlabs/consumer/CacheService.java
@@ -2,23 +2,25 @@ package no.fintlabs.consumer;
 
 import lombok.RequiredArgsConstructor;
 import no.fintlabs.cache.CacheContainer;
+import no.fintlabs.cache.CacheManager;
+import no.fintlabs.consumer.config.ConsumerConfiguration;
 import no.fintlabs.reflection.ReflectionService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
+@ComponentScan("no.fintlabs.cache")
 public class CacheService {
 
     private final ReflectionService reflectionService;
 
     @Bean
-    public CacheContainer cacheContainer() {
-        CacheContainer cacheContainer = new CacheContainer();
+    public CacheContainer cacheContainer(ConsumerConfiguration configuration, CacheManager cacheManager) {
+        CacheContainer cacheContainer = new CacheContainer(configuration, cacheManager);
 
-        reflectionService.getResources().forEach((resource, idFields) -> {
-            idFields.forEach(idField -> cacheContainer.initializeCache(resource.toLowerCase(), idField.toLowerCase()));
-        });
+        reflectionService.getResources().forEach((resource, idField) -> cacheContainer.initializeCache(resource.toLowerCase()));
 
         return cacheContainer;
     }
