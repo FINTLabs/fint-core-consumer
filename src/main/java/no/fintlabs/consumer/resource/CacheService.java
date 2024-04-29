ff6abb6d3f732ba0f4db1dbc17c44f14f51d6a39
Hknots <hknots99@gmail.com>
2024-04-29 13:30:24 +0200

    use FintCache

--- src/main/java/no/fintlabs/consumer/resource/CacheService.java
+++ src/main/java/no/fintlabs/consumer/resource/CacheService.java
@@ -1,23 +1,31 @@
-package no.fintlabs.consumer;
+package no.fintlabs.consumer.resource;
 
-import lombok.RequiredArgsConstructor;
+import no.fint.model.FintResource;
+import no.fintlabs.cache.Cache;
 import no.fintlabs.cache.CacheContainer;
 import no.fintlabs.cache.CacheManager;
 import no.fintlabs.consumer.config.ConsumerConfiguration;
 import no.fintlabs.reflection.ReflectionService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 @ComponentScan("no.fintlabs.cache")
 public class CacheService {
 
     private final ReflectionService reflectionService;
+    private final CacheContainer cacheContainer;
 
-    @Bean
-    public CacheContainer cacheContainer(ConsumerConfiguration configuration, CacheManager cacheManager) {
+    public CacheService(ReflectionService reflectionService, ConsumerConfiguration configuration, CacheManager cacheManager) {
+        this.reflectionService = reflectionService;
+        this.cacheContainer = createCacheContainer(configuration, cacheManager);
+    }
+
+    public Cache<FintResource> getCache(String resource) {
+        return cacheContainer.getCache(resource);
+    }
+
+    private CacheContainer createCacheContainer(ConsumerConfiguration configuration, CacheManager cacheManager) {
         CacheContainer cacheContainer = new CacheContainer(configuration, cacheManager);
 
         reflectionService.getResources().forEach((resource, idField) -> cacheContainer.initializeCache(resource.toLowerCase()));
