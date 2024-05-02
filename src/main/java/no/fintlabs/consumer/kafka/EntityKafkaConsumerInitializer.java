0956002bdc7e360e329db8a7c957ae051558b438
Hknots <hknots99@gmail.com>
2024-05-02 14:02:02 +0200

    sleep if bean is not found and try again

--- src/main/java/no/fintlabs/consumer/kafka/EntityKafkaConsumerInitializer.java
+++ src/main/java/no/fintlabs/consumer/kafka/EntityKafkaConsumerInitializer.java
@@ -1,26 +1,34 @@
-//package no.fintlabs.consumer.kafka;
-//
-//import jakarta.annotation.PostConstruct;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import no.fintlabs.reflection.ReflectionService;
-//import org.springframework.context.support.GenericApplicationContext;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//@Slf4j
-//public class EntityKafkaConsumerInitializer {
-//
-//    private final ReflectionService reflectionService;
-//    private final GenericApplicationContext applicationContext;
-//
-//    @PostConstruct
-//    public void init() {
-//        reflectionService.getResources().forEach((k, v) -> {
-//            Object bean = applicationContext.getBean(k + "EntityKafkaConsumer");
-//            log.info("created bean: {}", bean.getClass().getSimpleName());
-//        });
-//    }
-//
-//}
+package no.fintlabs.consumer.kafka;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import no.fintlabs.reflection.ReflectionService;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EntityKafkaConsumerInitializer {
+
+    private final ReflectionService reflectionService;
+    private final GenericApplicationContext applicationContext;
+
+    @PostConstruct
+    public void init() {
+        reflectionService.getResources().forEach((k, v) -> {
+            String beanName = k + "EntityKafkaConsumer";
+            log.info("created bean: {}", beanName);
+            while (!applicationContext.isBeanNameInUse(beanName)) {
+                try {
+                    log.info("Bean name: {} is not in use", beanName);
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+
+}
