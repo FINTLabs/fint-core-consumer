2b3c2a04a63e3b795c848d64d793843dfde3b733
Hknots <hknots99@gmail.com>
2024-05-16 15:56:10 +0200

    componentscan all packages under no.fintlabs

--- src/main/java/no/fintlabs/consumer/Application.java
+++ src/main/java/no/fintlabs/consumer/Application.java
@@ -2,10 +2,12 @@ package no.fintlabs.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@ComponentScan("no.fintlabs")
 public class Application {
 
     public static void main(String[] args) {
