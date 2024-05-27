1c5408d2d968d4c69d33041705c4848974a2af35
Hknots <hknots99@gmail.com>
2024-05-27 23:17:23 +0200

    add fint-model-resource locally

---
+++ src/main/java/no/fint/model/resource/FintResource.java
@@ -0,0 +1,7 @@
+package no.fint.model.resource;
+
+import no.fint.model.FintMainObject;
+import no.fint.model.resource.FintLinks;
+
+public interface FintResource extends FintLinks, FintMainObject {
+}
