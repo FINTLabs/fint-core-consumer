1c0a4b99e1503ac8b0d1a4cbf3f18caad49fca47
Hknots <hknots99@gmail.com>
2024-05-27 23:18:20 +0200

    add concept of toResources

--- src/main/java/no/fintlabs/consumer/LinkService.java
+++ src/main/java/no/fintlabs/consumer/LinkService.java
@@ -2,14 +2,20 @@ package no.fintlabs.consumer;
 
 import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
+import no.fint.model.resource.FintLinks;
 import no.fint.model.resource.FintResource;
+import no.fint.model.resource.FintResources;
 import no.fint.model.resource.Link;
+import no.fint.model.resource.utdanning.larling.LarlingResource;
 import no.fintlabs.adapter.models.RequestFintEvent;
 import no.fintlabs.consumer.config.ConsumerConfiguration;
+import no.fintlabs.reflection.ReflectionService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +25,52 @@ public class LinkService {
     private String baseUrl;
 
     private final ConsumerConfiguration configuration;
+    private final ReflectionService reflectionService;
+
+    public FintResources toResources(String resourceName, Stream<LarlingResource> stream, int offset, int size, int totalItems) {
+        FintResources fintResources = new FintResources();
+//        stream.map(resource -> toResource(resourceName, resource)).forEach(fintResources::addResource);
+        return fintResources;
+    }
+
+//    public FintResource toResource(String resourceName, FintResource resource) {
+//        this.mapLinks(resourceName, resource);
+//        if (resource.getSelfLinks() != null) {
+//            resource.getSelfLinks().clear();
+//        }
+//
+//        Stream var10000 = this.getAllSelfHrefs(resource).filter(StringUtils::isNotBlank);
+//        FintLinkMapper var10001 = this.linkMapper;
+//        Objects.requireNonNull(var10001);
+//        var10000 = var10000.map(var10001::populateProtocol).map(Link::with);
+//        Objects.requireNonNull(resource);
+//        var10000.forEach(resource::addSelf);
+//        return resource;
+//    }
+
+    public void mapLinks(String resourceName, FintLinks resource) {
+        if (resource != null) {
+
+            resource.getLinks().forEach((relationName, links) -> {
+                if (Objects.nonNull(links)) {
+                    links.forEach(link -> {
+                        if (Objects.nonNull(link)) {
+                            link.setVerdi("%s/%s/%s".formatted(baseUrl, getRelationUrl(resourceName, relationName), link.getHref()));
+                        }
+                    });
+                }
+            });
+
+            // TODO: Check out what nestedResources returns, and if resourceName is compatible
+            resource.getNestedResources().forEach(s -> mapLinks(resourceName, s));
+        }
+
+    }
+
+    private String getRelationUrl(String resourceName, String relationName) {
+        return reflectionService.getResources().get(resourceName).relationLinks().get(relationName);
+    }
+
 
     public void removePlaceholders(FintResource fintResource) {
         if (fintResource != null) {
