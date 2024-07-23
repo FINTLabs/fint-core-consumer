f9ee109c27939dbcd8cf338b40d5d5c429e6431e
Hknots <hknots99@gmail.com>
2024-07-23 15:50:03 +0200

    add order of aspects

--- src/main/java/no/fintlabs/consumer/resource/aspect/IdentifierAspect.java
+++ src/main/java/no/fintlabs/consumer/resource/aspect/IdentifierAspect.java
@@ -7,12 +7,14 @@ import no.fintlabs.reflection.ReflectionService;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 @Slf4j
 @RequiredArgsConstructor
+@Order(1)
 public class IdentifierAspect {
 
     private final ReflectionService reflectionService;
--- src/main/java/no/fintlabs/consumer/resource/aspect/WriteableAspect.java
+++ src/main/java/no/fintlabs/consumer/resource/aspect/WriteableAspect.java
@@ -7,12 +7,14 @@ import no.fintlabs.reflection.ReflectionService;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 @Slf4j
 @RequiredArgsConstructor
+@Order(2)
 public class WriteableAspect {
 
     private final ReflectionService reflectionService;
