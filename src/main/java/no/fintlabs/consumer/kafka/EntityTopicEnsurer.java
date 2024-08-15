aa753cf176de25a3359df019ca8937b359a45bc5
Hknots <hknots99@gmail.com>
2024-08-15 09:17:04 +0200

    add component to enityName

--- src/main/java/no/fintlabs/consumer/kafka/EntityTopicEnsurer.java
+++ src/main/java/no/fintlabs/consumer/kafka/EntityTopicEnsurer.java
@@ -1,6 +1,7 @@
 package no.fintlabs.consumer.kafka;
 
 import lombok.extern.slf4j.Slf4j;
+import no.fintlabs.consumer.config.ConsumerConfiguration;
 import no.fintlabs.kafka.entity.topic.EntityTopicNameParameters;
 import no.fintlabs.kafka.entity.topic.EntityTopicService;
 import no.fintlabs.reflection.ReflectionService;
@@ -12,22 +13,27 @@ public class EntityTopicEnsurer {
 
     private final EntityTopicService entityTopicService;
     private final ReflectionService reflectionService;
+    private final ConsumerConfiguration configuration;
     private final static Long DAY_IN_MILLISECONDS = 86400000L;
 
-    public EntityTopicEnsurer(EntityTopicService entityTopicService, ReflectionService reflectionService) {
+    public EntityTopicEnsurer(EntityTopicService entityTopicService, ReflectionService reflectionService, ConsumerConfiguration configuration) {
         this.entityTopicService = entityTopicService;
         this.reflectionService = reflectionService;
+        this.configuration = configuration;
         ensureEntityTopics();
     }
 
     private void ensureEntityTopics() {
         reflectionService.getResources().keySet().forEach(entityName -> {
-            log.info("Creating Kafka entity topic for entity: {}", entityName);
             entityTopicService.ensureTopic(
                     EntityTopicNameParameters.builder()
                             .orgId("fintlabs-no")
                             .domainContext("fint-core")
-                            .resource(entityName)
+                            .resource("%s-%s-%s".formatted(
+                                    configuration.getDomain(),
+                                    configuration.getPackageName(),
+                                    entityName
+                            ))
                             .build(),
                     DAY_IN_MILLISECONDS
             );
