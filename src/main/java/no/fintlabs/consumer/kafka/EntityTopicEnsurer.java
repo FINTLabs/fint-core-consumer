3d9e9dd3b1b1f60f71512a3a0bf90a773db59281
Hknots <hknots99@gmail.com>
2024-08-15 09:08:59 +0200

    remove applicationTest & ensure topic with log

--- src/main/java/no/fintlabs/consumer/kafka/EntityTopicEnsurer.java
+++ src/main/java/no/fintlabs/consumer/kafka/EntityTopicEnsurer.java
@@ -1,23 +1,28 @@
 package no.fintlabs.consumer.kafka;
 
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.kafka.entity.topic.EntityTopicNameParameters;
 import no.fintlabs.kafka.entity.topic.EntityTopicService;
 import no.fintlabs.reflection.ReflectionService;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+@Slf4j
 public class EntityTopicEnsurer {
 
     private final EntityTopicService entityTopicService;
     private final ReflectionService reflectionService;
     private final static Long DAY_IN_MILLISECONDS = 86400000L;
 
-    @PostConstruct
+    public EntityTopicEnsurer(EntityTopicService entityTopicService, ReflectionService reflectionService) {
+        this.entityTopicService = entityTopicService;
+        this.reflectionService = reflectionService;
+        ensureEntityTopics();
+    }
+
     private void ensureEntityTopics() {
         reflectionService.getResources().keySet().forEach(entityName -> {
+            log.info("Creating Kafka entity topic for entity: {}", entityName);
             entityTopicService.ensureTopic(
                     EntityTopicNameParameters.builder()
                             .orgId("fintlabs-no")
