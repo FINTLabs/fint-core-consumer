1b4f2b42761b5585baf268f465ff3e7766bb07b0
Hknots <hknots99@gmail.com>
2024-08-15 13:13:00 +0200

    ensure topic

--- src/main/java/no/fintlabs/consumer/kafka/LinkErrorProducer.java
+++ src/main/java/no/fintlabs/consumer/kafka/LinkErrorProducer.java
@@ -1,27 +1,31 @@
 package no.fintlabs.consumer.kafka;
 
+import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.kafka.event.error.Error;
 import no.fintlabs.kafka.event.error.ErrorCollection;
 import no.fintlabs.kafka.event.error.ErrorEventProducer;
 import no.fintlabs.kafka.event.error.ErrorEventProducerRecord;
 import no.fintlabs.kafka.event.error.topic.ErrorEventTopicNameParameters;
+import no.fintlabs.kafka.event.error.topic.ErrorEventTopicService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 
 @Service
+@Slf4j
 public class LinkErrorProducer {
 
     private final ErrorEventProducer eventProducer;
     private final ErrorEventTopicNameParameters errorEventTopicName;
+    private final static Long DAY_IN_MILLISECONDS = 86400000L;
 
-    public LinkErrorProducer(ErrorEventProducer eventProducer) {
+    public LinkErrorProducer(ErrorEventProducer eventProducer, ErrorEventTopicService errorEventTopicService) {
         this.eventProducer = eventProducer;
         this.errorEventTopicName = ErrorEventTopicNameParameters.builder()
                 .errorEventName("consumer-link")
                 .build();
-
+        errorEventTopicService.ensureTopic(errorEventTopicName, DAY_IN_MILLISECONDS);
     }
 
     public void publishError(String resourceLink, String errorLink, String errorMessage) {
