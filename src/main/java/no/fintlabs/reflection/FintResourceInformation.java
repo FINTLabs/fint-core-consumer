6f7fbd7573b12f288faaec5af1b0d1332c17eca6
Hknots <hknots99@gmail.com>
2024-08-18 17:45:37 +0200

    add static method to create FintResourceInformation

--- src/main/java/no/fintlabs/reflection/FintResourceInformation.java
+++ src/main/java/no/fintlabs/reflection/FintResourceInformation.java
@@ -1,10 +1,12 @@
 package no.fintlabs.reflection;
 
+import no.fint.model.FintModelObject;
 import no.fint.model.FintRelation;
 import no.fint.model.resource.FintResource;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public record FintResourceInformation(
         Class<? extends FintResource> clazz,
@@ -12,4 +14,14 @@ public record FintResourceInformation(
         List<FintRelation> relations,
         boolean isWriteable
 ) {
+
+    public static FintResourceInformation byMetaData(Class<? extends FintResource> resourceClass, FintModelObject fintModelObject) {
+        return new FintResourceInformation(
+                resourceClass,
+                fintModelObject.getIdentifikators().keySet().stream().map(String::toLowerCase).collect(Collectors.toSet()),
+                fintModelObject.getRelations(),
+                fintModelObject.isWriteable()
+        );
+    }
+
 }
