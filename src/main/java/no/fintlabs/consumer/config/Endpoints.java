1fa56f02489827fb8356fee183a78e1f9a8db4c6
Hknots <hknots99@gmail.com>
2024-08-28 09:50:45 +0200

    remove default endpoint

--- src/main/java/no/fintlabs/consumer/config/Endpoints.java
+++ src/main/java/no/fintlabs/consumer/config/Endpoints.java
@@ -2,10 +2,7 @@ package no.fintlabs.consumer.config;
 
 public class Endpoints {
 
-
-    public static final String DEFAULT_ENDPOINT = "${fint.consumer.domain}/${fint.consumer.package}";
-    public static final String ADMIN = DEFAULT_ENDPOINT + "/admin";
-
+    public static final String ADMIN = "/admin";
     public static final String LAST_UPDATED = "/last-updated";
     public static final String CACHE_SIZE = "/cache/size";
     public static final String BY_ID = "/{idField}/{idValue}";
