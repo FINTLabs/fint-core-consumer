9cce93941f0b52386abdb3f07d686195507e2e36
Hknots <hknots99@gmail.com>
2024-08-28 14:00:55 +0200

    rename class & store idfields of relations

--- src/main/java/no/fintlabs/consumer/links/RelationLinkIdFieldValidator.java
+++ src/main/java/no/fintlabs/consumer/links/RelationLinkIdFieldValidator.java
@@ -1,21 +1,27 @@
 package no.fintlabs.consumer.links;
 
 import lombok.extern.slf4j.Slf4j;
+import no.fint.model.FintRelation;
 import no.fintlabs.reflection.ResourceContext;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 @Slf4j
-public class LinkIdentificatorValidator {
+public class RelationLinkIdFieldValidator {
 
     private final Map<String, Map<String, Set<String>>> resourceLinkIdMap = new HashMap<>();
 
-    public LinkIdentificatorValidator(ResourceContext resourceContext) {
+    public RelationLinkIdFieldValidator(ResourceContext resourceContext) {
         setresourceLinkIds(resourceContext);
+    }
+
+    public boolean relationContainsIdField(String resourceName, String relationName, String idField) {
+        return resourceLinkIdMap.get(resourceName).get(relationName).contains(idField);
     }
 
     private void setresourceLinkIds(ResourceContext resourceContext) {
@@ -23,11 +29,17 @@ public class LinkIdentificatorValidator {
             resourceInformation.relations().forEach(fintRelation ->
                     resourceLinkIdMap.put(resourceName, Map.of(
                                     fintRelation.getName(),
-                                    resourceContext.getFintResourceInformationMap().get(fintRelation.getPackageName()).idFieldNames()
+                                    convertIdFieldsToLowercase(resourceContext, fintRelation)
                             )
                     )
             );
         });
+    }
+
+    private Set<String> convertIdFieldsToLowercase(ResourceContext resourceContext, FintRelation fintRelation) {
+        return resourceContext.getFintRelationInformationMap().get(fintRelation.getPackageName()).idFields().stream()
+                .map(String::toLowerCase)
+                .collect(Collectors.toSet());
     }
 
 }
