a57310442f655f54d126eeef289e003a4b0960c8
Hknots <hknots99@gmail.com>
2024-09-02 13:57:22 +0200

    refactor tests to support rewrite

--- src/test/java/no/fintlabs/consumer/links/LinkValidatorTest.java
+++ src/test/java/no/fintlabs/consumer/links/LinkValidatorTest.java
@@ -1,109 +1,145 @@
-//package no.fintlabs.consumer.links;
-//
-//import no.fint.model.resource.Link;
-//import no.fintlabs.consumer.exception.LinkError;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//import static org.mockito.Mockito.when;
-//
-//@ExtendWith(MockitoExtension.class)
-//public class LinkValidatorTest {
-//
-//    @Mock
-//    private RelationLinkIdFieldValidator relationLinkIdFieldValidator;
-//
-//    @InjectMocks
-//    private LinkValidator linkValidator;
-//
-//    @Test
-//    void validateLinkSuccess() {
-//        Link link = Link.with("value");
-//        linkValidator.validLink(link);
-//    }
-//
-//    @Test
-//    void validateLinkFailureWhenLinkIsNull() {
-//        Link link = null;
-//        assertThrows(LinkError.class, () -> {
-//            linkValidator.validLink(link);
-//        });
-//    }
-//
-//    @Test
-//    void validateLinkFailureWhenLinkHrefIsNull() {
-//        Link link = Link.with(null);
-//        assertThrows(LinkError.class, () -> {
-//            linkValidator.validLink(link);
-//        });
-//    }
-//
-//    @Test
-//    void validateIdFieldSuccess() {
-//        String resourceName = "testResource";
-//        String relationName = "testRelationName";
-//        String idField = "systemid";
-//        Link link = Link.with("systemid/123");
-//
-//        when(relationLinkIdFieldValidator.relationContainsIdField(resourceName, relationName, idField)).thenReturn(true);
-//
-//        linkValidator.validateIdField(resourceName, relationName, idField, link);
-//    }
-//
-//    @Test
-//    void validateIdFieldFailureWhenIdFieldDoesntMatchRelation() {
-//        String resourceName = "testResource";
-//        String relationName = "testRelationName";
-//        String idField = "systemid";
-//        Link link = Link.with("example/123");
-//
-//        when(relationLinkIdFieldValidator.relationContainsIdField(resourceName, relationName, idField)).thenReturn(false);
-//
-//        assertThrows(LinkError.class, () -> {
-//            linkValidator.validateIdField(resourceName, relationName, idField, link);
-//        });
-//    }
-//
-//    @Test
-//    void validateSegmentsSuccess() {
-//        String[] linkSegments = {"idField", "idValue"};
-//        Link link = Link.with("dontMatterToThisTest");
-//
-//        linkValidator.segmentsIsValid(linkSegments, link);
-//    }
-//
-//    @Test
-//    void validateSegmentsFailureWhenTooFewSegments() {
-//        String[] linkSegments = {"idValue"};
-//        Link link = Link.with("dontMatterToThisTest");
-//
-//        assertThrows(LinkError.class, () -> {
-//            linkValidator.segmentsIsValid(linkSegments, link);
-//        });
-//    }
-//
-//    @Test
-//    void validateSegmentsFailureWhenIdFieldSegmentIsEmpty() {
-//        String[] linkSegments = {"", "idValue"};
-//        Link link = Link.with("dontMatterToThisTest");
-//
-//        assertThrows(LinkError.class, () -> {
-//            linkValidator.segmentsIsValid(linkSegments, link);
-//        });
-//    }
-//
-//    @Test
-//    void validateSegmentsFailureWhenIdValueIsEmpty() {
-//        String[] linkSegments = {"idField", ""};
-//        Link link = Link.with("dontMatterToThisTest");
-//
-//        assertThrows(LinkError.class, () -> {
-//            linkValidator.segmentsIsValid(linkSegments, link);
-//        });
-//    }
-//
-//}
+package no.fintlabs.consumer.links;
+
+import no.fint.model.resource.Link;
+import no.fintlabs.consumer.exception.LinkError;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class LinkValidatorTest {
+
+    @Mock
+    private RelationLinkIdFieldValidator relationLinkIdFieldValidator;
+
+    @InjectMocks
+    private LinkValidator linkValidator;
+
+    @Test
+    void validateLinkSuccess() {
+        Link link = Link.with("value");
+        List<LinkError> linkErrors = new ArrayList<>();
+
+        boolean result = linkValidator.validLink(link, linkErrors);
+
+        assertTrue(result);
+        assertTrue(linkErrors.isEmpty());
+    }
+
+    @Test
+    void validateLinkFailureWhenLinkIsNull() {
+        Link link = null;
+        List<LinkError> linkErrors = new ArrayList<>();
+
+        boolean result = linkValidator.validLink(link, linkErrors);
+
+        assertFalse(result);
+        assertFalse(linkErrors.isEmpty());
+    }
+
+    @Test
+    void validateLinkFailureWhenLinkHrefIsNull() {
+        Link link = Link.with(null);
+        List<LinkError> linkErrors = new ArrayList<>();
+
+        boolean result = linkValidator.validLink(link, linkErrors);
+
+        assertFalse(result);
+        assertFalse(linkErrors.isEmpty());
+    }
+
+    @Test
+    void validateIdFieldSuccess() {
+        String resourceName = "testResource";
+        String relationName = "testRelationName";
+        String idField = "systemid";
+        List<LinkError> linkErrors = new ArrayList<>();
+
+        when(relationLinkIdFieldValidator.relationContainsIdField(resourceName, relationName, idField)).thenReturn(true);
+
+        boolean result = linkValidator.validateIdField(resourceName, relationName, idField, linkErrors);
+
+        assertTrue(result);
+        assertTrue(linkErrors.isEmpty());
+    }
+
+    @Test
+    void validateIdFieldFailureWhenIdFieldDoesntMatchRelation() {
+        String resourceName = "testResource";
+        String relationName = "testRelationName";
+        String idField = "systemid";
+        List<LinkError> linkErrors = new ArrayList<>();
+
+        when(relationLinkIdFieldValidator.relationContainsIdField(resourceName, relationName, idField)).thenReturn(false);
+
+        boolean validIdField = linkValidator.validateIdField(resourceName, relationName, idField, linkErrors);
+
+        assertFalse(validIdField);
+        assertFalse(linkErrors.isEmpty());
+    }
+
+    @Test
+    void validateSegmentsSuccess() {
+        String[] linkSegments = {"idField", "idValue"};
+        List<LinkError> linkErrors = new ArrayList<>();
+
+        boolean result = linkValidator.segmentsIsValid(linkSegments, linkErrors);
+
+        assertTrue(result);
+        assertTrue(linkErrors.isEmpty());
+    }
+
+    @Test
+    void validateSegmentsFailureWhenTooFewSegments() {
+        String[] linkSegments = {"idValue"};
+        List<LinkError> linkErrors = new ArrayList<>();
+
+        boolean result = linkValidator.segmentsIsValid(linkSegments, linkErrors);
+
+        assertFalse(result);
+        assertFalse(linkErrors.isEmpty());
+    }
+
+    @Test
+    void validateSegmentsFailureWhenIdFieldSegmentIsEmpty() {
+        String[] linkSegments = {"", "idValue"};
+        List<LinkError> linkErrors = new ArrayList<>();
+
+        boolean result = linkValidator.segmentsIsValid(linkSegments, linkErrors);
+
+        assertFalse(result);
+        assertFalse(linkErrors.isEmpty());
+    }
+
+    @Test
+    void validateSegmentsFailureWhenIdValueIsEmpty() {
+        String[] linkSegments = {"idField", ""};
+        List<LinkError> linkErrors = new ArrayList<>();
+
+        boolean result = linkValidator.segmentsIsValid(linkSegments, linkErrors);
+
+        assertFalse(result);
+        assertFalse(linkErrors.isEmpty());
+    }
+
+    @Test
+    void validateSegmentsShouldCreateTwoLinkErrorsIfBothSegmentsAreEmpty() {
+        String[] linkSegments = {"", ""};
+        List<LinkError> linkErrors = new ArrayList<>();
+
+        boolean result = linkValidator.segmentsIsValid(linkSegments, linkErrors);
+
+        assertFalse(result);
+        assertFalse(linkErrors.isEmpty());
+        assertEquals(linkErrors.size(), 2);
+    }
+
+}
