1c8ab9de7cb01fbce8657a3475324f658b2069c3
Hknots <hknots99@gmail.com>
2024-09-13 08:36:03 +0200

    add ResourceMapperTest

---
+++ src/test/java/no/fintlabs/consumer/resource/ResourceMapperTest.java
@@ -0,0 +1,30 @@
+package no.fintlabs.consumer.resource;
+
+import no.fint.model.felles.kompleksedatatyper.Identifikator;
+import no.fint.model.resource.FintResource;
+import no.fint.model.resource.Link;
+import no.fint.model.resource.utdanning.vurdering.ElevfravarResource;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class ResourceMapperTest {
+
+    @Autowired
+    private ResourceMapper resourceMapper;
+
+    @Test
+    public void mapResourceSuccess() {
+        ElevfravarResource elevfravarResource = new ElevfravarResource();
+        Identifikator identifikator = new Identifikator();
+        identifikator.setIdentifikatorverdi("123321");
+        elevfravarResource.addElevforhold(Link.with("test/link"));
+
+        FintResource fintResource = resourceMapper.mapResource("elevfravar", elevfravarResource);
+        assertEquals(fintResource.getLinks().get("elevforhold").getFirst().getHref(), "test/link");
+    }
+
+}
