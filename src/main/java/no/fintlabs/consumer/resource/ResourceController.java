a27055bfb09cad229502ae91310e6519cc3f27cf
Hknots <hknots99@gmail.com>
2024-10-14 17:53:30 +0200

    turn resource to lowercase

--- src/main/java/no/fintlabs/consumer/resource/ResourceController.java
+++ src/main/java/no/fintlabs/consumer/resource/ResourceController.java
@@ -35,7 +35,7 @@ public class ResourceController {
                                      @RequestParam(defaultValue = "0") int size,
                                      @RequestParam(defaultValue = "0") int offset,
                                      @RequestParam(defaultValue = "0") long sinceTimeStamp) {
-        return resourceService.getResources(resource, size, offset, sinceTimeStamp);
+        return resourceService.getResources(resource.toLowerCase(), size, offset, sinceTimeStamp);
     }
 
     @IdFieldCheck
@@ -43,40 +43,40 @@ public class ResourceController {
     public ResponseEntity<FintResource> getResourceById(@PathVariable String resource,
                                                         @PathVariable String idField,
                                                         @PathVariable String idValue) {
-        return resourceService.getResourceById(resource, idField, idValue)
+        return resourceService.getResourceById(resource.toLowerCase(), idField, idValue)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping(LAST_UPDATED)
     public Map<String, Long> getLastUpdated(@PathVariable String resource) {
-        return Map.of("lastUpdated", cacheService.getCache(resource).getLastUpdated());
+        return Map.of("lastUpdated", cacheService.getCache(resource.toLowerCase()).getLastUpdated());
     }
 
     @GetMapping(CACHE_SIZE)
     public Map<String, Integer> getResourceCacheSize(@PathVariable String resource) {
-        return Map.of("size", cacheService.getCache(resource).size());
+        return Map.of("size", cacheService.getCache(resource.toLowerCase()).size());
     }
 
     @WriteableResource
     @GetMapping(STATUS_ID)
     public ResponseEntity<Void> getStatus(@PathVariable String resource, @PathVariable String corrId) {
         return eventService.responseRecieved(corrId)
-                ? ResponseEntity.created(URI.create(eventService.createSelfHref(resource, corrId))).build()
+                ? ResponseEntity.created(URI.create(eventService.createSelfHref(resource.toLowerCase(), corrId))).build()
                 : ResponseEntity.accepted().build();
     }
 
     @WriteableResource
     @PostMapping
     public ResponseEntity<Void> postResource(@PathVariable String resource, @RequestBody Object resourceData) {
-        RequestFintEvent requestFintEvent = eventProducer.sendEvent(resource, resourceData, OperationType.CREATE);
+        RequestFintEvent requestFintEvent = eventProducer.sendEvent(resource.toLowerCase(), resourceData, OperationType.CREATE);
         return ResponseEntity.created(URI.create(eventService.getStatusHref(requestFintEvent))).build();
     }
 
     @WriteableResource
     @PutMapping
     public ResponseEntity<Void> putResource(@PathVariable String resource, @RequestBody Object resourceData) {
-        RequestFintEvent requestFintEvent = eventProducer.sendEvent(resource, resourceData, OperationType.UPDATE);
+        RequestFintEvent requestFintEvent = eventProducer.sendEvent(resource.toLowerCase(), resourceData, OperationType.UPDATE);
         return ResponseEntity.created(URI.create(eventService.getStatusHref(requestFintEvent))).build();
     }
 
