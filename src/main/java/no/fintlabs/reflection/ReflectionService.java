871647f7b3046d3462082f28a9502caa00991432
Hknots <hknots99@gmail.com>
2024-11-07 00:29:30 +0100

    save reference packageNAmes and simpleNAmes

--- src/main/java/no/fintlabs/reflection/ReflectionService.java
+++ src/main/java/no/fintlabs/reflection/ReflectionService.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import no.fint.model.FintAbstractObject;
 import no.fint.model.FintModelObject;
+import no.fint.model.FintReference;
 import no.fint.model.resource.FintResource;
 import org.reflections.Reflections;
 import org.springframework.stereotype.Service;
@@ -22,9 +23,31 @@ public class ReflectionService {
     private final Map<String, Class<? extends FintModelObject>> packageMetaSubTypeMap = createPackageMetaSubTypeMap();
     private final Map<String, Class<? extends FintResource>> packageResourceSubTypeMap = createPackageResourceSubTypeMap();
     private final Set<String> abstractPackageNames = createAbstractPackageNames();
+    private final Set<String> referencePackageNames = createReferencePackageNames();
+    private final Set<String> referenceSimpleNames = createReferenceSimpleNames();
 
     public boolean packageIsNotAbstract(String packageName) {
         return !abstractPackageNames.contains(packageName);
+    }
+
+    public boolean packageIsNotAReference(String packageName) {
+        return !referencePackageNames.contains(packageName);
+    }
+
+    public boolean relationNameIsNotAReference(String relationName) {
+        return !referenceSimpleNames.contains(relationName.toLowerCase());
+    }
+
+    private Set<String> createReferencePackageNames() {
+        return new Reflections("no.fint.model").getSubTypesOf(FintReference.class).stream()
+                .map(Class::getName)
+                .collect(Collectors.toSet());
+    }
+
+    private Set<String> createReferenceSimpleNames() {
+        return new Reflections("no.fint.model").getSubTypesOf(FintReference.class).stream()
+                .map(clazz -> clazz.getSimpleName().toLowerCase())
+                .collect(Collectors.toSet());
     }
 
     private Set<String> createAbstractPackageNames() {
