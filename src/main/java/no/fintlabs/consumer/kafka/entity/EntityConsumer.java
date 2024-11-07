80b31950bf8ceb2d7b806387b71ddf27f8a86940
Hknots <hknots99@gmail.com>
2024-11-07 15:12:33 +0100

    use EntityLoggingService on each consumtion of a record

--- src/main/java/no/fintlabs/consumer/kafka/entity/EntityConsumer.java
+++ src/main/java/no/fintlabs/consumer/kafka/entity/EntityConsumer.java
@@ -1,5 +1,6 @@
 package no.fintlabs.consumer.kafka.entity;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.fint.model.resource.FintResource;
@@ -11,7 +12,6 @@ import no.fintlabs.kafka.entity.EntityConsumerFactoryService;
 import no.fintlabs.kafka.entity.topic.EntityTopicNamePatternParameters;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 
@@ -22,6 +22,7 @@ public class EntityConsumer {
 
     private final ResourceService resourceService;
     private final ResourceMapper resourceMapper;
+    private final EntityLoggingService entityLoggingService;
 
     @Bean
     public ConcurrentMessageListenerContainer<String, Object> concurrentMessageListenerContainer(EntityConsumerFactoryService entityConsumerFactoryService,
@@ -38,8 +39,8 @@ public class EntityConsumer {
     }
 
     private void consumeRecord(ConsumerRecord<String, Object> consumerRecord) {
-        log.info("Consumed an entity: {}", consumerRecord.key());
         String resourceName = getResourceNameFromTopic(consumerRecord.topic());
+        entityLoggingService.startLogging(resourceName);
         FintResource fintResource = resourceMapper.mapResource(resourceName, consumerRecord.value());
         resourceService.addResourceToCache(resourceName, consumerRecord.key(), fintResource);
     }
