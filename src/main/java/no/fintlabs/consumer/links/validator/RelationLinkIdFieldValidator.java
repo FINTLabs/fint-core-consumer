eee191a9681e92c1df3f2a5ab2a20b0318df60e3
Hknots <hknots99@gmail.com>
2024-11-11 09:40:01 +0100

    refactor RelationLinkIdFieldValidator

--- src/main/java/no/fintlabs/consumer/links/validator/RelationLinkIdFieldValidator.java
+++ src/main/java/no/fintlabs/consumer/links/validator/RelationLinkIdFieldValidator.java
@@ -1,8 +1,10 @@
 package no.fintlabs.consumer.links.validator;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.fint.model.FintRelation;
-import no.fintlabs.reflection.ReflectionService;
+import no.fintlabs.reflection.FintResourceInformation;
 import no.fintlabs.reflection.ResourceContext;
 import org.springframework.stereotype.Component;
 
@@ -13,16 +15,15 @@ import java.util.stream.Collectors;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class RelationLinkIdFieldValidator {
 
-    private final ReflectionService reflectionService;
     private final Map<String, Map<String, Set<String>>> resourceLinkIdMap = new HashMap<>();
     private final ResourceContext resourceContext;
 
-    public RelationLinkIdFieldValidator(ReflectionService reflectionService, ResourceContext resourceContext) {
-        this.reflectionService = reflectionService;
-        this.resourceContext = resourceContext;
-        setresourceLinkIds();
+    @PostConstruct
+    private void init() {
+        setResourceLinkIds();
     }
 
     public boolean relationContainsIdField(String resourceName, String relationName, String idField) {
@@ -40,20 +41,26 @@ public class RelationLinkIdFieldValidator {
         return false;
     }
 
-    private void setresourceLinkIds() {
+    private void setResourceLinkIds() {
         resourceContext.getResources().forEach(resource -> {
             resourceLinkIdMap.put(resource.name(), new HashMap<>());
-            resource.relations().forEach(fintRelation -> {
-                String packageName = fintRelation.getPackageName();
-                if (reflectionService.packageIsNotAbstract(packageName) && reflectionService.packageIsNotAReference(packageName)) {
-                            resourceLinkIdMap.get(resource.name()).put(
-                                    fintRelation.getName().toLowerCase(),
-                                    convertIdFieldsToLowercase(fintRelation)
-                            );
-                        }
-                    }
-            );
+            resource.relations().forEach(fintRelation -> processRelation(resource, fintRelation));
         });
+    }
+
+    private void processRelation(FintResourceInformation resource, FintRelation fintRelation) {
+        String packageName = fintRelation.getPackageName();
+
+        if (shouldAddRelation(packageName)) {
+            resourceLinkIdMap.get(resource.name()).put(
+                    fintRelation.getName().toLowerCase(),
+                    convertIdFieldsToLowercase(fintRelation)
+            );
+        }
+    }
+
+    private boolean shouldAddRelation(String packageName) {
+        return resourceContext.notAbstract(packageName) && resourceContext.notFintReference(packageName);
     }
 
     private Set<String> convertIdFieldsToLowercase(FintRelation fintRelation) {
