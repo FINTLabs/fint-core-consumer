443c9451f88f201f02bd4201fd59bf092aa6acdf
Hknots <hknots99@gmail.com>
2024-11-11 09:09:01 +0100

    update FintResources

--- src/main/java/no/fint/model/resource/FintResources.java
+++ src/main/java/no/fint/model/resource/FintResources.java
@@ -3,14 +3,19 @@ package no.fint.model.resource;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class FintResources extends AbstractCollectionResources<FintResource> {
 
-
+    public static FintResources byStream(Stream<FintResource> resourceStream) {
+        FintResources fintResources = new FintResources();
+        resourceStream.forEach(fintResources::addResource);
+        return fintResources;
+    }
 
     @Override
     public TypeReference<List<FintResource>> getTypeReference() {
-        return null;
+        return new TypeReference<>() {};
     }
 
 }
