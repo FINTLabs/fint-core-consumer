b87e6207ac20e84065069d898ac57f6570b7b4d5
Hknots <hknots99@gmail.com>
2024-11-13 11:59:01 +0100

    add LinkServiceTest

---
+++ src/test/integration/java/no/fintlabs/consumer/links/LinkServiceTest.java
@@ -0,0 +1,249 @@
+package no.fintlabs.consumer.links;
+
+import no.fint.model.felles.kompleksedatatyper.Identifikator;
+import no.fint.model.resource.Link;
+import no.fint.model.resource.utdanning.elev.BasisgruppeResource;
+import no.fint.model.resource.utdanning.elev.ElevResource;
+import no.fintlabs.consumer.kafka.link.LinkErrorProducer;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest
+public class LinkServiceTest {
+
+    @MockBean
+    private LinkErrorProducer linkErrorProducer;
+
+    @Autowired
+    private LinkService linkService;
+
+    private final String elevResourceName = "elev";
+    private final String baseUrl = "https://test.felleskomponent.no";
+    private final String elevComponentUrl = baseUrl + "/utdanning/elev";
+    private final String utdanningsprogramUrl = baseUrl + "/utdanning/utdanningsprogram";
+    private final String elevResourceUrl = elevComponentUrl + "/elev";
+
+    @Test
+    void shouldCreateSelfLinks() {
+        ElevResource elevResource = createElevResource("123");
+
+        linkService.mapLinks(elevResourceName, elevResource);
+
+        assertEquals("%s/systemid/123".formatted(elevResourceUrl), elevResource.getSelfLinks().getFirst().getHref());
+    }
+
+    @Test
+    void shouldGenerateLinkToOtherComponent_WhenRelationBelongsToOtherComponent() {
+        BasisgruppeResource basisgruppeResource = createBasisgruppe("123");
+        basisgruppeResource.addSkole(Link.with("systemid/123"));
+
+        linkService.mapLinks("basisgruppe", basisgruppeResource);
+
+        assertEquals(
+                "%s/skole/systemid/123".formatted(utdanningsprogramUrl),
+                basisgruppeResource.getSkole().getFirst().getHref()
+        );
+    }
+
+    @Test
+    void shouldResetSelfLinks() {
+        ElevResource elevResource = createElevResource("123");
+        elevResource.addSelf(Link.with("shouldnt/exist"));
+        elevResource.addSelf(Link.with("shouldnt/exist/either"));
+
+        linkService.mapLinks(elevResourceName, elevResource);
+
+        assertEquals(1, elevResource.getSelfLinks().size());
+    }
+
+    @Test
+    void shouldRemoveRelation_WhenAllRelationLinksAreNull() {
+        ElevResource elevResource = createElevResource("123");
+        String relationName = "test";
+
+        elevResource.addLink(relationName, null);
+        elevResource.addLink(relationName, null);
+
+        linkService.mapLinks(elevResourceName, elevResource);
+
+        assertNull(elevResource.getLinks().get(relationName));
+    }
+
+    @Test
+    void shouldRemoveRelation_WhenAllRelationLinksHrefAreNull() {
+        ElevResource elevResource = createElevResource("123");
+        String relationName = "test";
+
+        elevResource.addLink(relationName, Link.with(null));
+        elevResource.addLink(relationName, Link.with(null));
+
+        linkService.mapLinks(elevResourceName, elevResource);
+
+        assertNull(elevResource.getLinks().get(relationName));
+    }
+
+    @Test
+    void shouldRemoveNullLinks() {
+        ElevResource elevResource = createElevResource("123");
+        String relationName = "test";
+
+        elevResource.addLink(relationName, null);
+        elevResource.addLink(relationName, Link.with("123"));
+
+        linkService.mapLinks(elevResourceName, elevResource);
+
+        assertEquals(1, elevResource.getLinks().get(relationName).size());
+    }
+
+    @Test
+    void shouldRemoveLinksWithHrefAsNull() {
+        ElevResource elevResource = createElevResource("123");
+        String relationName = "test";
+
+        elevResource.addLink(relationName, Link.with(null));
+        elevResource.addLink(relationName, Link.with("123"));
+
+        linkService.mapLinks(elevResourceName, elevResource);
+
+        assertEquals(1, elevResource.getLinks().get(relationName).size());
+    }
+
+    @Test
+    void shouldNotProcessLink_WhenRelationIsAReference() {
+        BasisgruppeResource basisgruppeResource = createBasisgruppe("123");
+        basisgruppeResource.addGrepreferanse(Link.with("https://non-processed-link.com"));
+
+        linkService.mapLinks("basisgruppe", basisgruppeResource);
+
+        assertEquals("https://non-processed-link.com", basisgruppeResource.getGrepreferanse().getFirst().getHref());
+    }
+
+    @Test
+    void shouldGenerateRelationLink_WhenLinkSegmentIsValid_1() {
+        ElevResource elevResource = createElevResource("123");
+        String linkSegment = "/systemid/123";
+        elevResource.addElevforhold(Link.with(linkSegment));
+
+        linkService.mapLinks(elevResourceName, elevResource);
+
+        assertEquals(
+                "%s/elevforhold/systemid/123".formatted(elevComponentUrl),
+                elevResource.getElevforhold().getFirst().getHref()
+        );
+    }
+
+    @Test
+    void shouldGenerateRelationLink_WhenLinkSegmentIsValid_2() {
+        ElevResource elevResource = createElevResource("123");
+        String linkSegment = "systemid/123";
+        elevResource.addElevforhold(Link.with(linkSegment));
+
+        linkService.mapLinks(elevResourceName, elevResource);
+
+        assertEquals(
+                "%s/elevforhold/systemid/123".formatted(elevComponentUrl),
+                elevResource.getElevforhold().getFirst().getHref()
+        );
+    }
+
+    @Test
+    void shouldGenerateRelationLink_WhenLinkSegmentIsValid_3() {
+        ElevResource elevResource = createElevResource("123");
+        String linkSegment = "https://no-valid-url.com/whatever/ok/systemid/123";
+        elevResource.addElevforhold(Link.with(linkSegment));
+
+        linkService.mapLinks(elevResourceName, elevResource);
+
+        assertEquals(
+                "%s/elevforhold/systemid/123".formatted(elevComponentUrl),
+                elevResource.getElevforhold().getFirst().getHref()
+        );
+    }
+
+    @Test
+    void shouldPublishErrors_WhenLinkHrefIsNull() {
+        ElevResource elevResource = createElevResource("123");
+        elevResource.addLink("test", Link.with(null));
+
+        linkService.mapLinks(elevResourceName, elevResource);
+
+        Mockito.verify(linkErrorProducer, Mockito.times(1))
+                .publishErrors(Mockito.anyList(), Mockito.anyList());
+    }
+
+    @Test
+    void shouldPublishErrors_WhenLinkIsNull() {
+        ElevResource elevResource = createElevResource("123");
+        elevResource.addLink("test", null);
+
+        linkService.mapLinks(elevResourceName, elevResource);
+
+        Mockito.verify(linkErrorProducer, Mockito.times(1))
+                .publishErrors(Mockito.anyList(), Mockito.anyList());
+    }
+
+    @Test
+    void shouldPublishErrors_WhenLinkListIsNull() {
+        ElevResource elevResource = createElevResource("123");
+        elevResource.getLinks().put("test", null);
+
+        linkService.mapLinks(elevResourceName, elevResource);
+
+        Mockito.verify(linkErrorProducer, Mockito.times(1))
+                .publishErrors(Mockito.anyList(), Mockito.anyList());
+    }
+
+    @Test
+    void shouldPublishErrors_WhenSegmentIsInvalid() {
+        ElevResource elevResource = createElevResource("123");
+        elevResource.addPerson(Link.with("longLinkButNotEnoughSegments"));
+
+        linkService.mapLinks(elevResourceName, elevResource);
+
+        Mockito.verify(linkErrorProducer, Mockito.times(1))
+                .publishErrors(Mockito.anyList(), Mockito.anyList());
+    }
+
+    @Test
+    void shouldPublishErrors_WhenIdFieldIsWrong() {
+        ElevResource elevResource = createElevResource("123");
+        elevResource.addPerson(Link.with("invalidIdField/123"));
+
+        linkService.mapLinks(elevResourceName, elevResource);
+
+        Mockito.verify(linkErrorProducer, Mockito.times(1))
+                .publishErrors(Mockito.anyList(), Mockito.anyList());
+    }
+
+    private BasisgruppeResource createBasisgruppe(String id) {
+        BasisgruppeResource basisgruppeResource = new BasisgruppeResource();
+
+        Identifikator identifikator = new Identifikator();
+        identifikator.setIdentifikatorverdi(id);
+        basisgruppeResource.setSystemId(identifikator);
+
+        basisgruppeResource.addTrinn(Link.with("systemid/123"));
+        basisgruppeResource.addSkole(Link.with("systemid/123"));
+
+        return basisgruppeResource;
+    }
+
+    private ElevResource createElevResource(String id) {
+        ElevResource elevResource = new ElevResource();
+
+        Identifikator identifikator = new Identifikator();
+        identifikator.setIdentifikatorverdi(id);
+        elevResource.setSystemId(identifikator);
+
+        elevResource.addPerson(Link.with("systemid/123"));
+
+        return elevResource;
+    }
+
+}
