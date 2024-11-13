c63ebd8b3d3e320fda8c6bdfb5c22c15e6c41be1
Hknots <hknots99@gmail.com>
2024-11-13 10:52:31 +0100

    add relationName & linkHref field

--- src/main/java/no/fintlabs/consumer/exception/LinkError.java
+++ src/main/java/no/fintlabs/consumer/exception/LinkError.java
@@ -3,11 +3,17 @@ package no.fintlabs.consumer.exception;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public record LinkError(String errorMessage) {
+public record LinkError(
+        String relationName,
+        String errorMessage,
+        String linkHref
+) {
 
-    public LinkError(String errorMessage) {
+    public LinkError(String relationName, String errorMessage, String linkHref) {
+        this.relationName = relationName;
         this.errorMessage = errorMessage;
-        log.error(errorMessage);
+        this.linkHref = linkHref;
+        log.debug("relation: {} error: {} linkHref: {}", relationName, errorMessage, linkHref);
     }
 
 }
