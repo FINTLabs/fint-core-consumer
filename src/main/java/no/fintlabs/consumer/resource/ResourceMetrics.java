44c2a500fb466f995459c4f1cca5c6919d617b3d
Hknots <hknots99@gmail.com>
2024-11-14 15:00:08 +0100

    add ResourceMetrics

---
+++ src/main/java/no/fintlabs/consumer/resource/ResourceMetrics.java
@@ -0,0 +1,31 @@
+package no.fintlabs.consumer.resource;
+
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import no.fintlabs.cache.CacheService;
+import no.fintlabs.reflection.ResourceContext;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ResourceMetrics {
+
+    private final MeterRegistry meterRegistry;
+    private final CacheService cacheService;
+    private final ResourceContext resourceContext;
+
+    @PostConstruct
+    private void init() {
+        resourceContext.getResources().forEach(resource -> {
+            registerCacheSize(resource.name());
+        });
+    }
+
+    private void registerCacheSize(String resourceName) {
+        Gauge.builder("%s-cache-size".formatted(resourceName), () -> cacheService.getCache(resourceName).size())
+                .register(meterRegistry);
+    }
+
+}
