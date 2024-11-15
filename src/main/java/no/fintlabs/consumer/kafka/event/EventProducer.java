b2a081655d1dba988bd2634eee26fea3060fd0e8
Hknots <hknots99@gmail.com>
2024-11-15 11:09:13 +0100

    implement resourceService to map links when sending event

--- src/main/java/no/fintlabs/consumer/kafka/event/EventProducer.java
+++ src/main/java/no/fintlabs/consumer/kafka/event/EventProducer.java
@@ -3,9 +3,11 @@ package no.fintlabs.consumer.kafka.event;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import no.fint.model.resource.FintResource;
 import no.fintlabs.adapter.models.event.RequestFintEvent;
 import no.fintlabs.adapter.operation.OperationType;
 import no.fintlabs.consumer.config.ConsumerConfiguration;
+import no.fintlabs.consumer.resource.ResourceService;
 import no.fintlabs.kafka.event.EventProducerFactory;
 import no.fintlabs.kafka.event.EventProducerRecord;
 import no.fintlabs.kafka.event.topic.EventTopicNameParameters;
@@ -24,16 +26,19 @@ public class EventProducer {
     private final no.fintlabs.kafka.event.EventProducer<RequestFintEvent> eventProducer;
     private final EventTopicService eventTopicService;
     private final ConsumerConfiguration configuration;
+    private final ResourceService resourceService;
     private final Set<String> topics = new HashSet<>();
 
-    public EventProducer(EventProducerFactory eventProducerFactory, EventTopicService eventTopicService, ConsumerConfiguration configuration) {
+    public EventProducer(EventProducerFactory eventProducerFactory, EventTopicService eventTopicService, ConsumerConfiguration configuration, ResourceService resourceService) {
         eventProducer = eventProducerFactory.createProducer(RequestFintEvent.class);
         this.configuration = configuration;
         this.eventTopicService = eventTopicService;
+        this.resourceService = resourceService;
     }
 
     public RequestFintEvent sendEvent(String resourceName, Object resourceData, OperationType operationType) {
-        RequestFintEvent requestFintEvent = createRequestFintEvent(resourceName, resourceData, operationType);
+        FintResource fintResource = resourceService.mapResourceAndLinks(resourceName, resourceData);
+        RequestFintEvent requestFintEvent = createRequestFintEvent(resourceName, fintResource, operationType);
         String eventName = createEventName(requestFintEvent);
         EventTopicNameParameters eventTopicNameParameters = EventTopicNameParameters.builder().eventName(eventName).build();
 
