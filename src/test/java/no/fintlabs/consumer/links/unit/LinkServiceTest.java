8009bbe9658a84c902149be09f36cef36bb53306
Hknots <hknots99@gmail.com>
2024-11-18 12:04:05 +0100

    mock NestedLinkService

--- src/test/java/no/fintlabs/consumer/links/unit/LinkServiceTest.java
+++ src/test/java/no/fintlabs/consumer/links/unit/LinkServiceTest.java
@@ -4,6 +4,7 @@ import no.fint.model.resource.FintResource;
 import no.fint.model.resource.utdanning.vurdering.ElevfravarResource;
 import no.fintlabs.consumer.kafka.LinkErrorProducer;
 import no.fintlabs.consumer.links.*;
+import no.fintlabs.consumer.links.nested.NestedLinkService;
 import no.fintlabs.consumer.links.validator.LinkValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,6 +30,9 @@ public class LinkServiceTest {
 
     @Mock
     private LinkGenerator linkGenerator;
+
+    @Mock
+    private NestedLinkService nestedLinkService;
 
     @Mock
     private LinkErrorProducer linkErrorProducer;
