1de80947b05ec9a5dbb2b78ecc5a5572e662ee19
Hknots <hknots99@gmail.com>
2024-11-23 03:30:37 +0100

    delete ReflectionService & create ReflectionInitializer

---
+++ src/main/java/no/fintlabs/reflection/ReflectionInitializer.java
@@ -0,0 +1,39 @@
+package no.fintlabs.reflection;
+
+import lombok.extern.slf4j.Slf4j;
+import no.fint.model.FintAbstractObject;
+import no.fint.model.FintModelObject;
+import no.fint.model.FintReference;
+import no.fint.model.resource.FintResource;
+import org.reflections.Reflections;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.function.BiConsumer;
+
+@Slf4j
+@Service
+public class ReflectionInitializer {
+
+    public ReflectionInitializer(ReflectionCache reflectionCache) {
+        Reflections reflections = new Reflections("no.fint.model");
+        registerSubTypes(reflections, FintModelObject.class, (p, s) -> reflectionCache.addMetaSubtype(p, s));
+        registerSubTypes(reflections, FintResource.class, (p, s) -> reflectionCache.addResourceSubtype(p, s));
+        registerSubTypes(reflections, FintAbstractObject.class, (p, s) -> reflectionCache.addAbstractSubtype(p, s));
+        registerSubTypes(reflections, FintReference.class, (p, s) -> reflectionCache.addReferenceSubtype(p, s));
+    }
+
+    public FintModelObject initializeFintModelObject(Class<? extends FintModelObject> metaSubType) {
+        try {
+            return metaSubType.getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private <T> void registerSubTypes(Reflections reflections, Class<T> type, BiConsumer<String, Class<? extends T>> cacheAdder) {
+        Set<Class<? extends T>> subTypes = reflections.getSubTypesOf(type);
+        subTypes.forEach(subType -> cacheAdder.accept(subType.getName(), subType));
+    }
+
+}
