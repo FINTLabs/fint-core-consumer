1cb981370171cc3eebf40f04e0bcf7c6106158f3
Hknots <hknots99@gmail.com>
2024-12-03 09:10:23 +0100

    remove unused exception handling

--- src/main/java/no/fintlabs/consumer/resource/ResourceExceptionHandler.java
+++ src/main/java/no/fintlabs/consumer/resource/ResourceExceptionHandler.java
@@ -1,9 +1,6 @@
 package no.fintlabs.consumer.resource;
 
 import lombok.extern.slf4j.Slf4j;
-import no.fintlabs.consumer.exception.event.EventFailedException;
-import no.fintlabs.consumer.exception.event.EventNotFoundException;
-import no.fintlabs.consumer.exception.event.EventRejectedException;
 import no.fintlabs.consumer.exception.resource.IdentificatorNotFoundException;
 import no.fintlabs.consumer.exception.resource.ResourceNotFoundException;
 import no.fintlabs.consumer.exception.resource.ResourceNotWriteableException;
@@ -15,39 +12,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @Slf4j
 @ControllerAdvice
 public class ResourceExceptionHandler {
-
-    @ExceptionHandler(EventRejectedException.class)
-    public ResponseEntity<?> eventRejected(EventRejectedException ex) {
-        log.debug(
-                "Event Status: 400 BAD REQUEST, Corr-id: {}, Reason: {}, Error Message: {}",
-                ex.getCorrId(),
-                "Event was rejected",
-                ex.getMessage()
-        );
-        return ResponseEntity.badRequest().body(ex.getMessage());
-    }
-
-    @ExceptionHandler(EventNotFoundException.class)
-    public ResponseEntity<?> eventRejected(EventNotFoundException ex) {
-        log.debug(
-                "Event Status: 404 NOT FOUND, Corr-id: {}, Reason: {}, Error Message: {}",
-                ex.getCorrId(),
-                "Event does not exist",
-                ex.getMessage()
-        );
-        return ResponseEntity.notFound().build();
-    }
-
-    @ExceptionHandler(EventFailedException.class)
-    public ResponseEntity<?> eventFailed(EventFailedException ex) {
-        log.debug(
-                "Event Status: 500 INTERNAL SERVER ERROR, Corr-id: {}, Reason: {}, Error Message: {}",
-                ex.getCorrId(),
-                "Event failed",
-                ex.getMessage()
-        );
-        return ResponseEntity.internalServerError().body(ex.getMessage());
-    }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> resourceNotFound(ResourceNotFoundException ex) {
