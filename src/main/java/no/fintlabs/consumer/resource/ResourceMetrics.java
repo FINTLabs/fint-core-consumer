bc82268b21069c7a8e61a28c855606d7136dbf14
Hknots <hknots99@gmail.com>
2025-01-16 14:25:18 +0100

    rename metric

--- src/main/java/no/fintlabs/consumer/resource/ResourceMetrics.java
+++ src/main/java/no/fintlabs/consumer/resource/ResourceMetrics.java
@@ -5,7 +5,6 @@ import io.micrometer.core.instrument.MeterRegistry;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import no.fintlabs.cache.CacheService;
-import no.fintlabs.consumer.config.ConsumerConfiguration;
 import no.fintlabs.consumer.resource.context.ResourceContext;
 import org.springframework.stereotype.Component;
 
@@ -16,20 +15,15 @@ public class ResourceMetrics {
     private final MeterRegistry meterRegistry;
     private final CacheService cacheService;
     private final ResourceContext resourceContext;
-    private final ConsumerConfiguration configuration;
 
     @PostConstruct
     private void init() {
-        resourceContext.getResources().forEach(resource -> {
-            registerCacheSize(resource.name());
-        });
+        resourceContext.getResources().forEach(resource -> registerCacheSize(resource.name()));
     }
 
     private void registerCacheSize(String resourceName) {
-        Gauge.builder(
-                "%s-%s-%s-cache-size".formatted(configuration.getDomain(), configuration.getPackageName(), resourceName),
-                () -> cacheService.getCache(resourceName).size()
-        ).register(meterRegistry);
+        Gauge.builder("%s-cache-size".formatted(resourceName), () -> cacheService.getCache(resourceName).size())
+                .register(meterRegistry);
     }
 
 }
