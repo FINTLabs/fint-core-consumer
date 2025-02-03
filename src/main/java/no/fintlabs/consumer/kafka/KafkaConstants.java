aea461c955afd82cd27830e91262e3172f96afd6
Hknots <hknots99@gmail.com>
2025-02-03 08:26:04 +0100

    feat: add KafkaConstants

---
+++ src/main/java/no/fintlabs/consumer/kafka/KafkaConstants.java
@@ -0,0 +1,9 @@
+package no.fintlabs.consumer.kafka;
+
+public class KafkaConstants {
+
+    // Don't "fix" the values, as they need to match the provider
+    public static String ENTITY_RETENTION_TIME = "entity-retension-time";
+    public static String TOPIC_RETENTION_TIME = "topic-retension-time";
+
+}
