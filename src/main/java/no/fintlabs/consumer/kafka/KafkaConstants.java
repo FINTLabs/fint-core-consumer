84e5e083fb130746a8b5e7cba0b24602042a826a
Hknots <hknots99@gmail.com>
2025-02-03 10:44:20 +0100

    fix: change constants

--- src/main/java/no/fintlabs/consumer/kafka/KafkaConstants.java
+++ src/main/java/no/fintlabs/consumer/kafka/KafkaConstants.java
@@ -2,8 +2,7 @@ package no.fintlabs.consumer.kafka;
 
 public class KafkaConstants {
 
-    // Don't "fix" the values, as they need to match the provider
-    public static String ENTITY_RETENTION_TIME = "entity-retension-time";
-    public static String TOPIC_RETENTION_TIME = "topic-retension-time";
+    public static String ENTITY_RETENTION_TIME = "entity-retention-time";
+    public static String TOPIC_RETENTION_TIME = "topic-retention-time";
 
 }
