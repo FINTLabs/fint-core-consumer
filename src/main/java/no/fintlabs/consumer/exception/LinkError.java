2ccbdb5b95fbc0231e260e6533baaa69fb50612e
Hknots <hknots99@gmail.com>
2025-02-04 12:30:38 +0100

    chore: remove redundant logging

--- src/main/java/no/fintlabs/consumer/exception/LinkError.java
+++ src/main/java/no/fintlabs/consumer/exception/LinkError.java
@@ -1,13 +1,9 @@
 package no.fintlabs.consumer.exception;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public record LinkError(String errorMessage) {
 
     public LinkError(String errorMessage) {
         this.errorMessage = errorMessage;
-        log.error(errorMessage);
     }
 
 }
