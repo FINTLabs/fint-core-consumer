f9b0c4bf434bc14f459466ea1ddfa8e9512c8b3b
Hknots <hknots99@gmail.com>
2025-03-19 11:49:06 +0100

    feat: trigger eviction upon a completed full sync event

---
+++ src/main/java/no/fintlabs/cache/CacheEvictionService.java
@@ -0,0 +1,47 @@
+package no.fintlabs.cache;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import no.fintlabs.adapter.models.event.ResourceEvictionPayload;
+import no.fintlabs.consumer.config.ConsumerConfiguration;
+import no.fintlabs.consumer.resource.context.ResourceContext;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CacheEvictionService {
+
+    private static final Long MINUTES_TO_WAIT = 10L;
+
+    private final TaskScheduler scheduler;
+    private final CacheService cacheService;
+    private final ResourceContext resourceContext;
+    private final ConsumerConfiguration configuration;
+
+    public void triggerEviction(ResourceEvictionPayload resourceEvictionPayload) {
+        if (payloadBelongsToThisConsumer(resourceEvictionPayload)) {
+            log.info("Eviction request triggered. Resource: {}", resourceEvictionPayload.getResource());
+            scheduler.schedule(
+                    () -> processEviction(resourceEvictionPayload),
+                    Instant.now().plus(Duration.ofMinutes(MINUTES_TO_WAIT))
+            );
+        }
+    }
+
+    private void processEviction(ResourceEvictionPayload resourceEvictionPayload) {
+        log.info("Eviction ongoing. Resource: {}", resourceEvictionPayload.getResource());
+        cacheService.getCache(resourceEvictionPayload.getResource().toLowerCase()).evictOldCacheObjects();
+    }
+
+    private boolean payloadBelongsToThisConsumer(ResourceEvictionPayload resourceEvictionPayload) {
+        return configuration.getDomain().equalsIgnoreCase(resourceEvictionPayload.getDomain())
+                && configuration.getPackageName().equalsIgnoreCase(resourceEvictionPayload.getPkg())
+                && resourceContext.getResourceNames().contains(resourceEvictionPayload.getResource().toLowerCase());
+    }
+
+}
---
+++ src/main/java/no/fintlabs/cache/CompletedFullSyncConsumer.java
@@ -0,0 +1,37 @@
+package no.fintlabs.cache;
+
+import lombok.RequiredArgsConstructor;
+import no.fintlabs.adapter.models.event.ResourceEvictionPayload;
+import no.fintlabs.kafka.event.EventConsumerFactoryService;
+import no.fintlabs.kafka.event.topic.EventTopicNameParameters;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CompletedFullSyncConsumer {
+
+    private final CacheEvictionService cacheEvictionService;
+    private final EventTopicNameParameters eventTopicName = createEventTopicNameParameters();
+
+    @Bean
+    public ConcurrentMessageListenerContainer<String, ResourceEvictionPayload> completedFullSyncListener(EventConsumerFactoryService eventConsumerFactoryService) {
+        return eventConsumerFactoryService.createFactory(ResourceEvictionPayload.class, this::consume)
+                .createContainer(eventTopicName);
+    }
+
+    private void consume(ConsumerRecord<String, ResourceEvictionPayload> consumerRecord) {
+        cacheEvictionService.triggerEviction(consumerRecord.value());
+    }
+
+    private EventTopicNameParameters createEventTopicNameParameters() {
+        return EventTopicNameParameters.builder()
+                .orgId("fintlabs-no")
+                .domainContext("fint-core")
+                .eventName("completed-full-sync")
+                .build();
+    }
+
+}
