40ca79bb20b83c1d9a704cb72678777a21f8bb72
Hknots <hknots99@gmail.com>
2025-03-20 12:28:57 +0100

    refactor: add domain, pkg and org to ConsumerError

--- src/main/java/no/fintlabs/consumer/exception/kafka/ConsumerError.java
+++ src/main/java/no/fintlabs/consumer/exception/kafka/ConsumerError.java
@@ -1,13 +1,21 @@
 package no.fintlabs.consumer.exception.kafka;
 
+import no.fintlabs.consumer.config.ConsumerConfiguration;
+
 public record ConsumerError(
+        String domain,
+        String pkg,
+        String org,
         String name,
         String error,
         StackTraceElement[] stacktrace,
         Long time
 ) {
-    public static ConsumerError fromException(Exception ex) {
+    public static ConsumerError from(Exception ex, ConsumerConfiguration configuration) {
         return new ConsumerError(
+                configuration.getDomain(),
+                configuration.getPackageName(),
+                configuration.getOrgId(),
                 ex.getClass().getSimpleName(),
                 ex.getMessage(),
                 ex.getStackTrace(),
