c6a6e9f9dd24e09e3e8e76245548fa71e4335915
Hknots <hknots99@gmail.com>
2025-03-20 11:47:14 +0100

    refactor: add name field

--- src/main/java/no/fintlabs/consumer/exception/kafka/ConsumerError.java
+++ src/main/java/no/fintlabs/consumer/exception/kafka/ConsumerError.java
@@ -1,17 +1,17 @@
 package no.fintlabs.consumer.exception.kafka;
 
 public record ConsumerError(
+        String name,
         String error,
         StackTraceElement[] stacktrace,
         Long time
 ) {
-
     public static ConsumerError fromException(Exception ex) {
         return new ConsumerError(
+                ex.getClass().getSimpleName(),
                 ex.getMessage(),
                 ex.getStackTrace(),
                 System.currentTimeMillis()
         );
     }
-
 }
