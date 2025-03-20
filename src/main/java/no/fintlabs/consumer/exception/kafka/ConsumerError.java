161cd37fb3eeae2f3af04b2fb094569bc9ecc5b2
Hknots <hknots99@gmail.com>
2025-03-20 09:44:14 +0100

    refactor: change stacktrace type

--- src/main/java/no/fintlabs/consumer/exception/kafka/ConsumerError.java
+++ src/main/java/no/fintlabs/consumer/exception/kafka/ConsumerError.java
@@ -2,7 +2,7 @@ package no.fintlabs.consumer.exception.kafka;
 
 public record ConsumerError(
         String error,
-        String stacktrace,
+        StackTraceElement[] stacktrace,
         Long time
 ) {
 }
