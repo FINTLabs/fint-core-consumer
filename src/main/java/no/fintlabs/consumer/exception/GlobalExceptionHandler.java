67ada8194171fa0a4740f9df4afdbf47e94c0262
Hknots <hknots99@gmail.com>
2025-03-20 12:30:12 +0100

    refactor/fix: add configratuon & set correct properties

--- src/main/java/no/fintlabs/consumer/exception/GlobalExceptionHandler.java
+++ src/main/java/no/fintlabs/consumer/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package no.fintlabs.consumer.exception;
 
 import lombok.RequiredArgsConstructor;
+import no.fintlabs.consumer.config.ConsumerConfiguration;
 import no.fintlabs.consumer.exception.kafka.ConsumerError;
 import no.fintlabs.consumer.exception.kafka.ConsumerErrorPublisher;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +14,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     private final ConsumerErrorPublisher consumerErrorPublisher;
+    private final ConsumerConfiguration configuration;
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleExceptions(Exception ex) {
-        consumerErrorPublisher.publish(ConsumerError.fromException(ex));
+        consumerErrorPublisher.publish(ConsumerError.from(ex, configuration));
         return ResponseEntity.internalServerError().build();
     }
 
