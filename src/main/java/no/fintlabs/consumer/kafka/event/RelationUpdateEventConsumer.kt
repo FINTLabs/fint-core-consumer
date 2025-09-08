eb530d14ebf7dcfb5f71a5613c48ec9cb804f887
Hknots <hknots99@gmail.com>
2025-09-08 19:08:37 +0200

    feat: add RelationService & RelationUpdateEventConsumer

---
+++ src/main/java/no/fintlabs/consumer/kafka/event/RelationUpdateEventConsumer.kt
@@ -0,0 +1,41 @@
+package no.fintlabs.consumer.kafka.event
+
+import no.fintlabs.autorelation.kafka.model.RelationUpdate
+import no.fintlabs.consumer.links.RelationService
+import no.fintlabs.kafka.event.EventConsumerConfiguration
+import no.fintlabs.kafka.event.EventConsumerFactoryService
+import no.fintlabs.kafka.event.topic.EventTopicNameParameters
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.context.annotation.Bean
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer
+import org.springframework.stereotype.Component
+
+@Component
+class RelationUpdateEventConsumer(
+    private val relationService: RelationService
+) {
+
+    @Bean
+    fun relationUpdateConsumer(
+        eventConsumerFactory: EventConsumerFactoryService,
+    ): ConcurrentMessageListenerContainer<String?, RelationUpdate> =
+        eventConsumerFactory.createFactory(
+            RelationUpdate::class.java,
+            this::consumeRecord,
+            EventConsumerConfiguration.builder()
+                .seekingOffsetResetOnAssignment(true)
+                .build()
+        ).createContainer(
+            EventTopicNameParameters.builder()
+                .orgId("fintlabs-no")
+                .domainContext("fint-core")
+                .eventName("relation-update")
+                .build()
+        )
+
+
+    fun consumeRecord(consumerRecord: ConsumerRecord<String, RelationUpdate>) =
+        relationService.processIfApplicable(consumerRecord.value())
+
+
+}
---
+++ src/main/java/no/fintlabs/consumer/links/RelationService.kt
@@ -0,0 +1,73 @@
+package no.fintlabs.consumer.links
+
+import no.fint.model.resource.FintResource
+import no.fint.model.resource.Link
+import no.fintlabs.autorelation.kafka.model.RelationOperation
+import no.fintlabs.autorelation.kafka.model.RelationRef
+import no.fintlabs.autorelation.kafka.model.RelationUpdate
+import no.fintlabs.autorelation.kafka.model.ResourceId
+import no.fintlabs.consumer.config.ConsumerConfiguration
+import no.fintlabs.consumer.resource.ResourceService
+import org.springframework.stereotype.Service
+import kotlin.jvm.optionals.getOrNull
+
+@Service
+class RelationService(
+    private val linkService: LinkService,
+    private val resourceService: ResourceService,
+    private val consumerConfig: ConsumerConfiguration
+) {
+
+    fun processIfApplicable(relationUpdate: RelationUpdate) =
+        relationUpdate.takeIf(::belongsToThisService)
+            ?.let(::processRelationUpdate)
+
+    fun processRelationUpdate(relationUpdate: RelationUpdate) =
+        getResource(relationUpdate)
+            ?.let { processRelation(relationUpdate, it) }
+            ?: println("resource not found")
+
+    private fun processRelation(relationUpdate: RelationUpdate, resource: FintResource): Unit =
+        resource.linksIfPresent
+            ?.getOrPut(relationUpdate.relation.name) { mutableListOf() }
+            ?.let { mutateRelation(relationUpdate, it) }
+            ?.let { linkService.mapLinks(relationUpdate.resource.name, resource) }
+            ?: println("Relation not found")
+
+    private fun mutateRelation(relationUpdate: RelationUpdate, links: MutableList<Link>) =
+        when (relationUpdate.operation) {
+            RelationOperation.ADD -> addRelations(links, relationUpdate.relation)
+            RelationOperation.DELETE -> deleteRelations(links, relationUpdate.relation)
+        }
+
+    private fun addRelations(links: MutableList<Link>, relationRef: RelationRef) =
+        relationRef.ids.forEach { id ->
+            if (!links.any { linkExists(it, id) })
+                links.add(Link.with(formatIdLink(id)))
+        }
+
+    private fun linkExists(link: Link, id: ResourceId) =
+        link.toString().endsWith("${id.field}/${id.value}", ignoreCase = true)
+
+    private fun deleteRelations(links: MutableList<Link>, relationRef: RelationRef) =
+        relationRef.ids.forEach { id ->
+            links.removeIf {
+                it.toString().endsWith(formatIdLink(id), ignoreCase = true)
+            }
+        }
+
+    private fun getResource(relationUpdate: RelationUpdate): FintResource? =
+        resourceService.getResourceById(
+            relationUpdate.resource.name,
+            relationUpdate.resource.id.field,
+            relationUpdate.resource.id.value
+        ).getOrNull()
+
+    private fun belongsToThisService(relationUpdate: RelationUpdate) =
+        consumerConfig.orgId.replace(".", "-").equals(relationUpdate.orgId, ignoreCase = true)
+                && consumerConfig.domain.equals(relationUpdate.domainName, ignoreCase = true)
+                && consumerConfig.packageName.equals(relationUpdate.packageName, ignoreCase = true)
+
+    private fun formatIdLink(id: ResourceId) = "${id.field}/${id.value}"
+
+}
