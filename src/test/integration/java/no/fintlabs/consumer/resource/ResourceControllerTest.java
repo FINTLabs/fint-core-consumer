a945c7bd44e495359fa76bb43d11e61c39f5a71e
Hknots <hknots99@gmail.com>
2025-10-01 09:26:50 +0200

    refactor: use MockitoBean instead of deprecated bean

--- src/test/integration/java/no/fintlabs/consumer/resource/ResourceControllerTest.java
+++ src/test/integration/java/no/fintlabs/consumer/resource/ResourceControllerTest.java
@@ -23,10 +23,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
@@ -52,7 +52,7 @@ public class ResourceControllerTest {
     @Autowired
     private CacheService cacheService;
 
-    @MockBean
+    @MockitoBean
     private EventProducer eventProducer;
 
     private static final String RESOURCENAME = "elevforhold";
