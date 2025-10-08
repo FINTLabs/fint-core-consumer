7cf8842857b4ee5f9aacd41ce97aab622f2b9726
Hknots <hknots99@gmail.com>
2025-10-08 12:39:45 +0200

    feat: add relation caffeine cache

--- src/main/java/no/fintlabs/consumer/config/CaffeineCacheConfig.java
+++ src/main/java/no/fintlabs/consumer/config/CaffeineCacheConfig.java
@@ -2,11 +2,13 @@ package no.fintlabs.consumer.config;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
-import no.fintlabs.adapter.models.event.RequestFintEvent;
+import no.fint.model.resource.Link;
 import no.fintlabs.adapter.models.event.ResponseFintEvent;
+import no.fintlabs.consumer.links.RelationKey;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -23,6 +25,13 @@ public class CaffeineCacheConfig {
     public Cache<String, ResponseFintEvent> responseFintEvents() {
         return Caffeine.newBuilder()
                 .expireAfterWrite(4, TimeUnit.HOURS)
+                .build();
+    }
+
+    @Bean
+    public Cache<RelationKey, List<Link>> relationLinkCache() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(2, TimeUnit.HOURS)
                 .build();
     }
 
