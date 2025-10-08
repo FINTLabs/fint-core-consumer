14fa0a6aa24993b110b3dd50e6456a58098cbae2
Hknots <hknots99@gmail.com>
2025-10-08 12:42:35 +0200

    feat: consume relation update entities from Kafka

---
+++ src/main/java/no/fintlabs/consumer/kafka/entity/RelationUpdateEntityConsumer.kt
@@ -0,0 +1,31 @@
+package no.fintlabs.consumer.kafka.entity
+
+import no.fintlabs.autorelation.model.RelationUpdate
+import no.fintlabs.consumer.links.RelationService
+import no.fintlabs.kafka.entity.EntityConsumerFactoryService
+import no.fintlabs.kafka.entity.topic.EntityTopicNameParameters
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
+
+@Component
+class RelationUpdateEntityConsumer(
+    private val relationService: RelationService
+) {
+
+    @Bean
+    fun entityConsumerFactory(consumerFactoryService: EntityConsumerFactoryService) =
+        consumerFactoryService
+            .createFactory(RelationUpdate::class.java, this::consumeRecord)
+            .createContainer(
+                EntityTopicNameParameters.builder()
+                    .orgId("fintlabs-no")
+                    .domainContext("fint-core")
+                    .resource("relation-update")
+                    .build()
+            )
+
+    fun consumeRecord(consumerRecord: ConsumerRecord<String, RelationUpdate>) =
+        relationService.processIfApplicable(consumerRecord.value())
+
+}
