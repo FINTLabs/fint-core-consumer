ef030a224be7d9e8af13cb8c0370267de2738d9c
Hknots <hknots99@gmail.com>
2025-10-08 12:42:02 +0200

    refactor: remove return types & cache links instead of queueing them

--- src/main/java/no/fintlabs/consumer/links/RelationService.kt
+++ src/main/java/no/fintlabs/consumer/links/RelationService.kt
@@ -8,18 +8,15 @@ import no.fintlabs.autorelation.model.RelationUpdate
 import no.fintlabs.autorelation.model.ResourceId
 import no.fintlabs.cache.CacheService
 import no.fintlabs.consumer.config.ConsumerConfiguration
-import no.fintlabs.consumer.kafka.entity.EntityProducer
 import org.slf4j.LoggerFactory
-import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 
 @Service
 class RelationService(
     private val linkService: LinkService,
     private val cacheService: CacheService,
-    private val entityProducer: EntityProducer,
-    private val publisher: ApplicationEventPublisher,
-    private val consumerConfig: ConsumerConfiguration
+    private val consumerConfig: ConsumerConfiguration,
+    private val relationCacheService: RelationCacheService
 ) {
 
     private val logger = LoggerFactory.getLogger(javaClass)
@@ -28,42 +25,51 @@ class RelationService(
         relationUpdate.takeIf(::belongsToThisService)
             ?.let(::processRelationUpdate)
 
-    fun processRelationUpdate(relationUpdate: RelationUpdate): Boolean =
+    fun processRelationUpdate(relationUpdate: RelationUpdate) =
         getResource(relationUpdate)
             ?.let { processRelation(relationUpdate, it) }
-            ?: run {
-                publisher.publishEvent(relationUpdate)
-                false
-            }
+            ?: run { registerLinks(relationUpdate) }
 
-    private fun processRelation(relationUpdate: RelationUpdate, resource: FintResource): Boolean =
+    fun addRelations(resource: String, resourceId: String, resourceObject: FintResource) =
+        resourceObject.links.forEach { (relationName, links) -> // Should we iterate over relations we shouldn't mutate? (worst case cache lookup O(1) ~200MB on 5 million relation lookups)
+            mutateLinks(links, relationCacheService.pollLinks(resource, resourceId, relationName))
+        }
+
+    private fun registerLinks(relationUpdate: RelationUpdate) =
+        relationCacheService.registerLinks(
+            resource = relationUpdate.resource.name,
+            resourceId = relationUpdate.resource.id.value,
+            relation = relationUpdate.relation.name,
+            links = createLinks(relationUpdate.relation)
+        )
+
+    private fun processRelation(relationUpdate: RelationUpdate, resource: FintResource) =
         resource.links
             ?.getOrPut(relationUpdate.relation.name) { mutableListOf() }
             ?.let { mutateRelation(relationUpdate, it) }
-            ?.let {
-                linkService.mapLinks(relationUpdate.resource.name, resource)
-                entityProducer.produceEntity(relationUpdate, resource)
-                true
-            }
-            ?: run {
-                logger.error("Unable to process relation update")
-                false
-            }
+            ?.let { linkService.mapLinks(relationUpdate.resource.name, resource) }
+            ?: run { logger.error("Unable to process relation update") }
 
     private fun mutateRelation(relationUpdate: RelationUpdate, links: MutableList<Link>) =
         when (relationUpdate.operation) {
-            RelationOperation.ADD -> addRelations(links, relationUpdate.relation)
+            RelationOperation.ADD -> mutateLinks(links, createLinks(relationUpdate.relation))
             RelationOperation.DELETE -> deleteRelations(links, relationUpdate.relation)
         }
 
-    private fun addRelations(links: MutableList<Link>, relationRef: RelationRef) =
-        relationRef.ids.forEach { id -> // TODO: Use a set instead of a list
-            if (!links.any { linkExists(it, id) })
-                links.add(Link.with(formatIdLink(id)))
+    private fun createLinks(relationRef: RelationRef): List<Link> =
+        relationRef.ids.map { Link.with("${it.field}/${it.value}") }
+
+    private fun mutateLinks(mutableLinks: MutableList<Link>, linksToAdd: List<Link>) =
+        linksToAdd.forEach { link ->
+            if (!mutableLinks.any { linkMatches(it, link) })
+                mutableLinks.add(link)
         }
 
-    private fun linkExists(link: Link, id: ResourceId) =
-        link.toString().endsWith("${id.field}/${id.value}", ignoreCase = true)
+    private fun linkMatches(existingLink: Link, idLink: Link) =
+        existingLink.href.endsWith(
+            idLink.href,
+            ignoreCase = true
+        ) // TODO: Replace List<Link> with Set<Link> or improved FintLinks data structure
 
     private fun deleteRelations(links: MutableList<Link>, relationRef: RelationRef) =
         relationRef.ids.forEach { id ->
