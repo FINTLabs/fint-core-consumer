1caa0f82e9626e8bd7887a43df90abda600aba36
Hknots <hknots99@gmail.com>
2025-10-13 10:50:01 +0200

    refactor: convert CompletedFullSyncConsumer to Kotlin & only consume based on correct configuration

--- src/main/java/no/fintlabs/cache/CompletedFullSyncConsumer.kt
+++ src/main/java/no/fintlabs/cache/CompletedFullSyncConsumer.kt
@@ -1,37 +1,34 @@
-package no.fintlabs.cache;
+package no.fintlabs.cache
 
-import lombok.RequiredArgsConstructor;
-import no.fintlabs.kafka.event.EventConsumerFactoryService;
-import no.fintlabs.kafka.event.topic.EventTopicNameParameters;
-import no.fintlabs.status.models.ResourceEvictionPayload;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.stereotype.Service;
+import no.fintlabs.consumer.config.ConsumerConfiguration
+import no.fintlabs.kafka.event.EventConsumerFactoryService
+import no.fintlabs.kafka.event.topic.EventTopicNameParameters
+import no.fintlabs.status.models.ResourceEvictionPayload
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
 
-@Service
-@RequiredArgsConstructor
-public class CompletedFullSyncConsumer {
-
-    private final CacheEvictionService cacheEvictionService;
-    private final EventTopicNameParameters eventTopicName = createEventTopicNameParameters();
+@Component
+class CompletedFullSyncConsumer(
+    private val consumerConfig: ConsumerConfiguration,
+    private val cacheEvictionService: CacheEvictionService
+) {
 
     @Bean
-    public ConcurrentMessageListenerContainer<String, ResourceEvictionPayload> completedFullSyncListener(EventConsumerFactoryService eventConsumerFactoryService) {
-        return eventConsumerFactoryService.createFactory(ResourceEvictionPayload.class, this::consume)
-                .createContainer(eventTopicName);
-    }
+    fun completedFullSyncContainer(eventConsumerFactoryService: EventConsumerFactoryService) =
+        eventConsumerFactoryService.createFactory(
+            ResourceEvictionPayload::class.java,
+            this::consume
+        ).createContainer(createEventTopic())
 
-    private void consume(ConsumerRecord<String, ResourceEvictionPayload> consumerRecord) {
-        cacheEvictionService.triggerEviction(consumerRecord.value());
-    }
+    private fun consume(consumerRecord: ConsumerRecord<String?, ResourceEvictionPayload>) =
+        consumerRecord.value().takeIf { consumerConfig.matchesConfiguration(it.domain, it.pkg, it.org) }
+            ?.let { cacheEvictionService.triggerEviction(it) }
 
-    private EventTopicNameParameters createEventTopicNameParameters() {
-        return EventTopicNameParameters.builder()
-                .orgId("fintlabs-no")
-                .domainContext("fint-core")
-                .eventName("completed-full-sync")
-                .build();
-    }
-
+    private fun createEventTopic(): EventTopicNameParameters =
+        EventTopicNameParameters.builder()
+            .orgId("fintlabs-no")
+            .domainContext("fint-core")
+            .eventName("completed-full-sync")
+            .build()
 }
