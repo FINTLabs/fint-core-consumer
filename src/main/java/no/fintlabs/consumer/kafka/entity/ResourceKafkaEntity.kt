e099454a29f3d15f4f556360957d6b00293faa8c
Hknots <hknots99@gmail.com>
2025-10-17 13:47:38 +0200

    refactor: remove persisted property and related method from ResourceKafkaEntity

--- src/main/java/no/fintlabs/consumer/kafka/entity/ResourceKafkaEntity.kt
+++ src/main/java/no/fintlabs/consumer/kafka/entity/ResourceKafkaEntity.kt
@@ -1,7 +1,6 @@
 package no.fintlabs.consumer.kafka.entity
 
 import no.fint.model.resource.FintResource
-import no.fintlabs.consumer.kafka.KafkaConstants.CONSUMER
 import no.fintlabs.consumer.kafka.KafkaConstants.ENTITY_RETENTION_TIME
 import no.fintlabs.consumer.kafka.KafkaHeader
 import org.apache.kafka.clients.consumer.ConsumerRecord
@@ -11,7 +10,6 @@ data class ResourceKafkaEntity(
     val key: String,
     val name: String,
     val resource: FintResource?,
-    val persisted: Boolean,
     val createdTime: Long?
 ) {
     companion object {
@@ -21,7 +19,6 @@ data class ResourceKafkaEntity(
                 name = resourceName,
                 key = record.key(),
                 resource = resource,
-                persisted = isPersisted(record.headers()),
                 createdTime = getCreatedTime(record.headers())
             )
 
@@ -29,7 +26,5 @@ data class ResourceKafkaEntity(
             headers.lastHeader(ENTITY_RETENTION_TIME)
                 ?.let { KafkaHeader.getLong(it) }
 
-        private fun isPersisted(headers: Headers) =
-            headers.lastHeader(CONSUMER) != null
     }
 }
