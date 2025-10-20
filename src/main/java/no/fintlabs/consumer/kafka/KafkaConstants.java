d7bf088b2bd688d3614212478debfa0ddeb8f5a1
Hknots <hknots99@gmail.com>
2025-10-20 09:36:06 +0200

    refactor: remove unused constants from KafkaConstants

--- src/main/java/no/fintlabs/consumer/kafka/KafkaConstants.java
+++ src/main/java/no/fintlabs/consumer/kafka/KafkaConstants.java
@@ -4,7 +4,5 @@ public class KafkaConstants {
 
     public static String ENTITY_RETENTION_TIME = "entity-retention-time";
     public static String TOPIC_RETENTION_TIME = "topic-retention-time";
-    public static String IS_TRUE_STATE = "is-true-state";
-    public static String CONSUMER = "consumer";
 
 }
