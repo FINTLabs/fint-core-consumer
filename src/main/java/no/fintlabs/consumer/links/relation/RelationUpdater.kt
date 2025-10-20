e9d191b1a02664a5647346a6dc52c4eaf5841177
Hknots <hknots99@gmail.com>
2025-10-20 09:39:18 +0200

    refactor: update relation mutation methods for improved clarity and functionality

--- src/main/java/no/fintlabs/consumer/links/relation/RelationUpdater.kt
+++ src/main/java/no/fintlabs/consumer/links/relation/RelationUpdater.kt
@@ -24,21 +24,23 @@ class RelationUpdater {
 
     private fun mutateRelation(relationUpdate: RelationUpdate, links: MutableList<Link>) =
         when (relationUpdate.operation) {
-            RelationOperation.ADD -> mutateLinks(links, relationUpdate.relation.links)
-            RelationOperation.DELETE -> deleteRelations(links, relationUpdate.relation)
+            RelationOperation.ADD -> addLinks(links, relationUpdate.relation.links)
+            RelationOperation.DELETE -> deleteLinks(links, relationUpdate.relation)
         }
 
-    private fun mutateLinks(mutableLinks: MutableList<Link>, linksToAdd: List<Link>) =
-        linksToAdd.forEach { link ->
-            if (!mutableLinks.any { linkMatches(it, link) })
-                mutableLinks.add(link)
-        }
-
-    private fun addLinks(target: MutableList<Link>, toAdd: List<Link>) {
+    fun addLinks(target: MutableList<Link>, toAdd: List<Link>) =
         toAdd.forEach { link ->
-            if (target.none { linkMatches(it, link) }) target.add(link)
+            if (target.none { linkMatches(it, link) })
+                target.add(link)
         }
-    }
+
+    fun addLinks(targetResource: FintResource, relation: String, toAdd: List<Link>) =
+        getRelationLinks(relation, targetResource)?.let { target ->
+            toAdd.forEach { link ->
+                if (target.none { linkMatches(it, link) })
+                    target.add(link)
+            }
+        }
 
     private fun linkMatches(existingLink: Link, idLink: Link) =
         existingLink.href.endsWith(
@@ -46,7 +48,7 @@ class RelationUpdater {
             ignoreCase = true
         ) // TODO: Replace List<Link> with Set<Link> or improved FintLinks data structure
 
-    private fun deleteRelations(links: MutableList<Link>, relationRef: RelationRef) =
+    fun deleteLinks(links: MutableList<Link>, relationRef: RelationRef) =
         relationRef.links.first { id ->
             links.removeIf { linkMatches(it, id) }
         }
