020cdc6dd9c5de148f85378a49563a9e44429736
Hknots <hknots99@gmail.com>
2025-10-20 11:08:25 +0200

    feat: enable configuration properties scanning and add eviction config

--- src/main/java/no/fintlabs/Application.java
+++ src/main/java/no/fintlabs/Application.java
@@ -3,11 +3,13 @@ package no.fintlabs;
 import no.fint.antlr.EnableFintFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFintFilter
 @EnableScheduling
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Application {
 
     public static void main(String[] args) {
