d8bfac97ec8f900e93dce00e2dc7bce67b9747d6
Hknots <hknots99@gmail.com>
2025-10-24 13:15:53 +0200

    feat: add utility extension functions for Kafka Headers

---
+++ src/main/java/no/fintlabs/consumer/kafka/HeaderExtensions.kt
@@ -0,0 +1,11 @@
+package no.fintlabs.consumer.kafka
+
+import org.apache.kafka.common.header.Header
+import org.apache.kafka.common.header.Headers
+
+internal fun Headers.require(name: String): Header =
+    lastHeader(name) ?: throw IllegalArgumentException("Missing header: $name")
+
+internal fun Headers.byte(name: String): Byte = KafkaHeader.getByte(require(name))
+internal fun Headers.long(name: String): Long = KafkaHeader.getLong(require(name))
+internal fun Headers.string(name: String): String = KafkaHeader.getString(require(name))
