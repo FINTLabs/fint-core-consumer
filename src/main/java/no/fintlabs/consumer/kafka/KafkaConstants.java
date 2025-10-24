1e359a0e037a6287d13dcb39610f2fe34b776e9c
Hknots <hknots99@gmail.com>
2025-10-24 11:47:33 +0200

    feat: add new constants for sync-related fields in KafkaConstants

--- src/main/java/no/fintlabs/consumer/kafka/KafkaConstants.java
+++ src/main/java/no/fintlabs/consumer/kafka/KafkaConstants.java
@@ -4,6 +4,10 @@ public class KafkaConstants {
 
     public static String ENTITY_RETENTION_TIME = "entity-retention-time";
     public static String TOPIC_RETENTION_TIME = "topic-retention-time";
+    public static String SYNC_CORRELATION_ID = "correlation-id";
+    public static String SYNC_TOTAL_SIZE = "sync-total-size";
+    public static String SYNC_TYPE = "sync-type";
+    public static String MODIFIED_TIME = ENTITY_RETENTION_TIME; // TODO: Deprecate ENTITY_RETENTION_TIME in the future, plan for removal in provider-gateway
     public static String IS_TRUE_STATE = "is-true-state";
     public static String CONSUMER = "consumer";
 
