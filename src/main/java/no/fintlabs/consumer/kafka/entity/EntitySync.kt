6277348689cf9658086093aecb0617ecdc2b7103
Hknots <hknots99@gmail.com>
2025-11-03 13:10:08 +0100

    refactor: enhance `EntitySync` data class with javadoc

--- src/main/java/no/fintlabs/consumer/kafka/entity/EntitySync.kt
+++ src/main/java/no/fintlabs/consumer/kafka/entity/EntitySync.kt
@@ -7,20 +7,27 @@ import no.fintlabs.consumer.kafka.long
 import no.fintlabs.consumer.kafka.string
 import org.apache.kafka.common.header.Headers
 
+/**
+ * Sync information for a resource event.
+ *
+ * Indicates whether it's a FULL, DELTA, or DELETE sync,
+ * including its correlation ID and expected total size.
+ */
 data class EntitySync(
     val type: SyncType,
     val corrId: String,
-    val totalSize: Long
+    val totalSize: Long,
 )
 
 fun createEntitySync(headers: Headers): EntitySync =
     EntitySync(
         type = headers.syncType(),
         corrId = headers.string(SYNC_CORRELATION_ID),
-        totalSize = headers.long(SYNC_TOTAL_SIZE)
+        totalSize = headers.long(SYNC_TOTAL_SIZE),
     )
 
 private fun Headers.syncType() =
-    this.byte(SYNC_TYPE)
+    this
+        .byte(SYNC_TYPE)
         .let { SyncType.entries.getOrNull(it.toInt()) }
         ?: throw IllegalArgumentException("Invalid SyncType index")
