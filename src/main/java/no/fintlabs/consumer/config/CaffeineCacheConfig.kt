40252d21357468c2880f56260dbf1e850c4ec729
Hknots <hknots99@gmail.com>
2025-11-06 15:08:57 +0100

    refactor: rewrite Caffeine cache configuration in Kotlin and introduce `CaffeineCacheProperties` for customizable cache expiration settings

--- src/main/java/no/fintlabs/consumer/config/CaffeineCacheConfig.kt
+++ src/main/java/no/fintlabs/consumer/config/CaffeineCacheConfig.kt
@@ -1,38 +1,58 @@
-package no.fintlabs.consumer.config;
+package no.fintlabs.consumer.config
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
-import no.fint.model.resource.Link;
-import no.fintlabs.adapter.models.event.ResponseFintEvent;
-import no.fintlabs.consumer.links.relation.RelationKey;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import com.github.benmanes.caffeine.cache.Cache
+import com.github.benmanes.caffeine.cache.Caffeine
+import com.github.benmanes.caffeine.cache.RemovalCause
+import no.fint.model.resource.Link
+import no.fintlabs.adapter.models.event.ResponseFintEvent
+import no.fintlabs.consumer.kafka.sync.SyncStatusProducer
+import no.fintlabs.consumer.kafka.sync.model.SyncKey
+import no.fintlabs.consumer.kafka.sync.model.SyncState
+import no.fintlabs.consumer.kafka.sync.model.createSyncStatus
+import no.fintlabs.consumer.links.relation.RelationKey
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.concurrent.TimeUnit
 
 @Configuration
-public class CaffeineCacheConfig {
+open class CaffeineCacheConfig(
+    private val syncStatusProducer: SyncStatusProducer,
+    private val caffeineCacheProperties: CaffeineCacheProperties,
+) {
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     @Bean
-    public Cache<String, String> stringCache() {
-        return Caffeine.newBuilder()
-                .expireAfterWrite(4, TimeUnit.HOURS)
-                .build();
-    }
+    open fun stringCache(): Cache<String, String> =
+        Caffeine
+            .newBuilder()
+            .expireAfterWrite(4, TimeUnit.HOURS)
+            .build()
 
     @Bean
-    public Cache<String, ResponseFintEvent> responseFintEvents() {
-        return Caffeine.newBuilder()
-                .expireAfterWrite(4, TimeUnit.HOURS)
-                .build();
-    }
+    open fun responseFintEvents(): Cache<String, ResponseFintEvent> =
+        Caffeine
+            .newBuilder()
+            .expireAfterWrite(4, TimeUnit.HOURS)
+            .build()
 
     @Bean
-    public Cache<RelationKey, List<Link>> relationLinkCache() {
-        return Caffeine.newBuilder()
-                .expireAfterWrite(2, TimeUnit.HOURS)
-                .build();
-    }
+    open fun relationLinkCache(): Cache<RelationKey, MutableList<Link>> =
+        Caffeine
+            .newBuilder()
+            .expireAfterWrite(2, TimeUnit.HOURS)
+            .build()
 
+    @Bean
+    open fun syncStateCache(): Cache<SyncKey, SyncState> =
+        Caffeine
+            .newBuilder()
+            .expireAfterAccess(caffeineCacheProperties.expireAfterAccess)
+            .removalListener { key: SyncKey?, state: SyncState?, cause: RemovalCause ->
+                if (key != null && state != null) {
+                    syncStatusProducer.publish(createSyncStatus(state.corrId, key.type, cause))
+                } else {
+                    logger.error("Null key or state when removing from cache: $key, $state, $cause")
+                }
+            }.build()
 }
