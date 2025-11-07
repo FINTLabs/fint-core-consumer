126784714d18b9c017355f6cf1013e5ca81ed841
Hknots <hknots99@gmail.com>
2025-11-07 08:03:47 +0100

    feat: integrate `SyncTrackerService` into `ResourceService` for entity sync tracking

--- src/main/java/no/fintlabs/consumer/resource/ResourceService.java
+++ src/main/java/no/fintlabs/consumer/resource/ResourceService.java
@@ -11,6 +11,7 @@ import no.fintlabs.cache.CacheService;
 import no.fintlabs.consumer.config.ConsumerConfiguration;
 import no.fintlabs.consumer.kafka.entity.KafkaEntity;
 import no.fintlabs.consumer.kafka.event.RelationRequestProducer;
+import no.fintlabs.consumer.kafka.sync.SyncTrackerService;
 import no.fintlabs.consumer.links.LinkService;
 import no.fintlabs.consumer.links.relation.RelationService;
 import org.apache.commons.lang3.StringUtils;
@@ -39,8 +40,10 @@ public class ResourceService {
     private final FintFilterService oDataFilterService;
     private final RelationRequestProducer relationRequestProducer;
     private final ConsumerConfiguration consumerConfiguration;
+    private final SyncTrackerService syncTrackerService;
 
     public void handleNewEntity(KafkaEntity kafkaEntity) {
+        syncTrackerService.recordSync(kafkaEntity.getName(), kafkaEntity.getSync());
         if (kafkaEntity.getResource() == null) deleteEntity(kafkaEntity);
         else addToCache(kafkaEntity);
     }
