e325c695d9e0a862bfb48931bf63e60d3729921f
Hknots <hknots99@gmail.com>
2025-11-11 09:53:26 +0100

    test: add integration tests for `ResourceService` cache eviction and last delivered handling

--- src/test/integration/java/no/fintlabs/consumer/resource/ResourceServiceTest.kt
+++ src/test/integration/java/no/fintlabs/consumer/resource/ResourceServiceTest.kt
@@ -4,12 +4,21 @@ import no.fint.model.felles.kompleksedatatyper.Identifikator
 import no.fint.model.resource.FintResource
 import no.fint.model.resource.Link
 import no.fint.model.resource.utdanning.elev.ElevResource
+import no.fintlabs.adapter.models.sync.SyncType
+import no.fintlabs.cache.CacheService
+import no.fintlabs.consumer.kafka.entity.EntitySync
+import no.fintlabs.consumer.kafka.entity.KafkaEntity
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertNotNull
+import org.junit.jupiter.api.assertNull
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.kafka.test.context.EmbeddedKafka
 import org.springframework.test.context.ActiveProfiles
+import java.time.Duration
+import java.util.*
+import kotlin.test.assertEquals
 
 @SpringBootTest
 @ActiveProfiles("utdanning-elev")
@@ -17,6 +26,53 @@ import org.springframework.test.context.ActiveProfiles
 class ResourceServiceTest {
     @Autowired
     private lateinit var resourceService: ResourceService
+
+    @Autowired
+    private lateinit var cacheService: CacheService
+
+    private val resourceName = "elev"
+
+    @Test
+    fun `ensure lastDelivered is set upon new resource`() {
+        val resourceId = UUID.randomUUID().toString()
+        val oneDayAgo = System.currentTimeMillis() - Duration.ofDays(1).toMillis()
+        val kafkaEntity = createKafkaEntity(resourceId, lastModified = oneDayAgo)
+
+        resourceService.handleNewEntity(kafkaEntity)
+
+        assertNotNull(getResourceFromCache(resourceId))
+        assertEquals(oneDayAgo, getLastDelivered(resourceId))
+    }
+
+    @Test
+    fun `ensure expired resource is not evicted upon cache eviction (set retention to 10 millis)`() {
+        val resourceId = UUID.randomUUID().toString()
+        val kafkaEntity = createKafkaEntity(resourceId, retentionTime = 10L)
+
+        resourceService.handleNewEntity(kafkaEntity)
+
+        assertNotNull(getResourceFromCache(resourceId))
+
+        triggerCacheEviction()
+        Thread.sleep(50)
+
+        assertNull(getResourceFromCache(resourceId))
+    }
+
+    @Test
+    fun `ensure non-expired resource is not evicted upon cache eviction (default retention is 7 days)`() {
+        val resourceId = UUID.randomUUID().toString()
+        val kafkaEntity = createKafkaEntity(resourceId)
+
+        resourceService.handleNewEntity(kafkaEntity)
+
+        assertNotNull(getResourceFromCache(resourceId))
+
+        triggerCacheEviction()
+        Thread.sleep(50)
+
+        assertNotNull(getResourceFromCache(resourceId))
+    }
 
     @Test
     fun mapResourceAndLinksSuccess() {
@@ -33,6 +89,33 @@ class ResourceServiceTest {
                 .href,
         )
     }
+
+    private fun triggerCacheEviction() = getCache().evictOldCacheObjects()
+
+    private fun getResourceFromCache(resourceId: String) = getCache().get(resourceId)
+
+    private fun getCache() = cacheService.getCache(resourceName)
+
+    private fun getLastDelivered(resourceId: String) = getCache().getLastDelivered(resourceId)
+
+    private fun createKafkaEntity(
+        resourceId: String,
+        resource: FintResource? = createElevResource(resourceId),
+        lastModified: Long = System.currentTimeMillis(),
+        retentionTime: Long? = null,
+    ) = KafkaEntity(
+        key = resourceId,
+        name = resourceName,
+        resource = resource,
+        lastModified = lastModified,
+        sync =
+            EntitySync(
+                type = SyncType.FULL,
+                corrId = UUID.randomUUID().toString(),
+                totalSize = 1L,
+            ),
+        retentionTime = retentionTime,
+    )
 
     private fun createElevResource(id: String?): ElevResource =
         ElevResource().apply {
