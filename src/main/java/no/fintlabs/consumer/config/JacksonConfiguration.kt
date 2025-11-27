233f5df5644deeab71333f86c136da3a846822b2
Hknots <hknots99@gmail.com>
2025-11-27 14:40:01 +0100

    refactor: introduce ISO-8601 Zulu pattern for date serialization

--- src/main/java/no/fintlabs/consumer/config/JacksonConfiguration.kt
+++ src/main/java/no/fintlabs/consumer/config/JacksonConfiguration.kt
@@ -3,7 +3,6 @@ package no.fintlabs.consumer.config
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.databind.util.StdDateFormat
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
@@ -13,6 +12,22 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 
 @Configuration
 open class JacksonConfiguration {
+    companion object {
+        /**
+         * ISO-8601 Zulu format required by Felles Personmodell → fødselsdato:
+         * https://informasjonsmodell.felleskomponent.no/docs/felles_person/~fodselsdato
+         * https://en.wikipedia.org/wiki/ISO_8601
+         *
+         * Must match EXACTLY:
+         *   YYYY-MM-DDThh:mm:ssZ
+         *
+         * - Always UTC
+         * - Always ends with 'Z'
+         * - No milliseconds
+         */
+        const val ISO_8601_ZULU_PATTERN = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+    }
+
     @Bean
     @Primary
     open fun jackson2ObjectMapperBuilder(): Jackson2ObjectMapperBuilder =
@@ -20,7 +35,7 @@ open class JacksonConfiguration {
             .failOnUnknownProperties(false)
             .serializationInclusion(JsonInclude.Include.NON_NULL)
             .modules(JavaTimeModule(), KotlinModule.Builder().build())
-            .dateFormat(StdDateFormat())
+            .simpleDateFormat(ISO_8601_ZULU_PATTERN)
             .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
 
     @Bean
