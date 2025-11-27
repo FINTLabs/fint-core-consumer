a77d8458be2c074aaf5d7bbb456e3a343a22630a
Hknots <hknots99@gmail.com>
2025-11-27 13:33:51 +0100

    refactor: replace repetitive test cases with parameterized tests in ConsumerConfigurationTest

--- src/test/java/no/fintlabs/consumer/config/ConsumerConfigurationTest.kt
+++ src/test/java/no/fintlabs/consumer/config/ConsumerConfigurationTest.kt
@@ -1,6 +1,8 @@
 package no.fintlabs.consumer.config
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.context.SpringBootTest
@@ -11,13 +13,15 @@ import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 @SpringBootTest(classes = [ConsumerConfigurationTest.PropertiesTestConfig::class])
-@TestPropertySource(properties = [
-    "fint.relation.base-url=https://testorg.no",
-    "fint.consumer.domain=domain",
-    //"fint.consumer.org-id=org",
-    "fint.org-id=foo.org",
-    "fint.consumer.package=package"
-])
+@TestPropertySource(
+    properties = [
+        "fint.relation.base-url=https://testorg.no",
+        "fint.consumer.domain=domain",
+        // "fint.consumer.org-id=org",
+        "fint.org-id=foo.org",
+        "fint.consumer.package=package",
+    ],
+)
 internal class ConsumerConfigurationTest {
     @Configuration
     @EnableConfigurationProperties(ConsumerConfiguration::class)
@@ -31,27 +35,41 @@ internal class ConsumerConfigurationTest {
         assertEquals("https://testorg.no/domain/package", consumerConfiguration.componentUrl)
     }
 
-    @Test
-    fun `matchesConfiguration is true when domain, package and orgId is correct, independent of case`() {
-        assertTrue(consumerConfiguration.matchesConfiguration("domain", "package", "foo.org"))
-        assertTrue(consumerConfiguration.matchesConfiguration("DOMAIN", "package", "foo.org"))
-        assertTrue(consumerConfiguration.matchesConfiguration("domain", "Package", "foo.org"))
-        assertTrue(consumerConfiguration.matchesConfiguration("domain", "package", "foo.ORG"))
-    }
+    @ParameterizedTest
+    @CsvSource(
+        "domain, package, foo.org",
+        "domAin, packagE, fOO.org",
+        "dOmain, PAckage, foo.OrG",
+    )
+    fun `matchesConfiguration is true when domain, package and orgId is correct, independent of case`(
+        domain: String,
+        pkg: String,
+        orgId: String,
+    ) = assertTrue(consumerConfiguration.matchesConfiguration(domain, pkg, orgId))
 
-    @Test
-    fun `matchesConfiguration is true when orgId contains dash or underscore instead of dot`() {
-        assertTrue(consumerConfiguration.matchesConfiguration("domain", "package", "foo_org"))
-        assertTrue(consumerConfiguration.matchesConfiguration("domain", "package", "foo_ORG"))
-        assertTrue(consumerConfiguration.matchesConfiguration("domain", "package", "foo-ORG"))
-    }
+    @ParameterizedTest
+    @CsvSource(
+        "domain, package, foo_org",
+        "domain, package, foo_ORG",
+        "domain, package, foo-ORG",
+    )
+    fun `matchesConfiguration is true when orgId contains dash or underscore instead of dot`(
+        domain: String,
+        pkg: String,
+        orgId: String,
+    ) = assertTrue(consumerConfiguration.matchesConfiguration(domain, pkg, orgId))
 
-    @Test
-    fun `matchesConfiguration is false when domain, package and orgId does not match`() {
-        assertFalse(consumerConfiguration.matchesConfiguration("another-domain", "package", "org"))
-        assertFalse(consumerConfiguration.matchesConfiguration("domain", "another-package", "org"))
-        assertFalse(consumerConfiguration.matchesConfiguration("domain", "package", "another-org"))
-        assertFalse(consumerConfiguration.matchesConfiguration("domain", "package", "foo..org"))
-        assertFalse(consumerConfiguration.matchesConfiguration("domain", "package", "foo--org"))
-    }
+    @ParameterizedTest
+    @CsvSource(
+        "another-domain, package, org",
+        "domain, another-package, org",
+        "domain, package, another-org",
+        "domain, package, foo..org",
+        "domain, package, foo--org",
+    )
+    fun `matchesConfiguration is false when domain, package and orgId does not match`(
+        domain: String,
+        pkg: String,
+        orgId: String,
+    ) = assertFalse(consumerConfiguration.matchesConfiguration(domain, pkg, orgId))
 }
