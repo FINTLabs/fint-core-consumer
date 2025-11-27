7cbd9113e35fe08ad939e1c6243e48c32871449b
Hknots <hknots99@gmail.com>
2025-11-27 14:17:35 +0100

    test: add deserialization test for Kotlin class in Jackson configuration integration tests

--- src/integrationTest/java/no/fintlabs/consumer/config/JacksonConfigurationTest.kt
+++ src/integrationTest/java/no/fintlabs/consumer/config/JacksonConfigurationTest.kt
@@ -1,10 +1,15 @@
 package no.fintlabs.consumer.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import no.fintlabs.autorelation.model.RelationOperation
+import no.fintlabs.autorelation.model.RelationRef
+import no.fintlabs.autorelation.model.RelationUpdate
+import no.fintlabs.autorelation.model.ResourceRef
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import org.springframework.test.context.TestPropertySource
 import java.util.*
 import kotlin.test.assertTrue
@@ -31,4 +36,25 @@ class JacksonConfigurationTest {
         val iso8601Regex = Regex("^\"\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}([+-]\\d{2}:\\d{2}|Z)\"$")
         assertTrue(iso8601Regex.matches(json), "Expected ISO-8601 format, but got: $json")
     }
+
+    @Test
+    fun `can deserialize RelationUpdate without throwing`() {
+        val relationUpdate = createRelationUpdate() // Kotlin class
+        val json = objectMapper.writeValueAsString(relationUpdate)
+
+        assertDoesNotThrow("Deserializing RelationUpdate should not throw when KotlinModule is configured") {
+            val deserialized = objectMapper.readValue(json, RelationUpdate::class.java)
+            assertEquals(relationUpdate.orgId, deserialized.orgId)
+        }
+    }
+
+    private fun createRelationUpdate() =
+        RelationUpdate(
+            orgId = "orgId",
+            domainName = "domainName",
+            packageName = "pkg",
+            resource = ResourceRef("asdf", "asdf"),
+            relation = RelationRef("asdf", emptyList()),
+            operation = RelationOperation.ADD,
+        )
 }
