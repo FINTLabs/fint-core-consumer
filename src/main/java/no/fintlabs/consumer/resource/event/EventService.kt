e815d85a7fd195260c5ae710b34a6bfbf4f89f97
daniejoh <github@danieljohnsen.no>
2025-12-09 11:00:49 +0100

    refactor: Rewrite EventService to kotlin

---
+++ src/main/java/no/fintlabs/consumer/resource/event/EventService.kt
@@ -0,0 +1,37 @@
+package no.fintlabs.consumer.resource.event
+
+import com.github.benmanes.caffeine.cache.Cache
+import lombok.RequiredArgsConstructor
+import lombok.extern.slf4j.Slf4j
+import no.fint.model.resource.FintResource
+import no.fintlabs.adapter.models.event.ResponseFintEvent
+import no.fintlabs.cache.CacheService
+import org.springframework.stereotype.Service
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+class EventService(
+    private val requestFintCorrIds: Cache<String, String>,
+    private val responseFintEvents: Cache<String, ResponseFintEvent>,
+    private val cacheService: CacheService,
+) {
+    fun registerResponse(
+        key: String,
+        response: ResponseFintEvent,
+    ) = responseFintEvents.put(key, response)
+
+    fun registerRequest(key: String) = requestFintCorrIds.put(key, key)
+
+    fun getResponse(corrId: String): ResponseFintEvent? = responseFintEvents.getIfPresent(corrId)
+
+    fun requestExists(corrId: String): Boolean = requestFintCorrIds.getIfPresent(corrId) != null
+
+    fun getResource(
+        resourceName: String,
+        corrId: String,
+    ): FintResource? =
+        responseFintEvents
+            .getIfPresent(corrId)
+            ?.let { cacheService.getCache(resourceName).get(it.value.identifier) }
+}
