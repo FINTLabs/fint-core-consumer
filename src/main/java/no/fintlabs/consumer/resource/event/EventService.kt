9a3fbbc153ee1735c416986426f179f3265be108
Hknots <hknots99@gmail.com>
2025-12-11 20:16:56 +0100

    refactor: remove unused CacheService and related methods from EventService to simplify class structure

--- src/main/java/no/fintlabs/consumer/resource/event/EventService.kt
+++ src/main/java/no/fintlabs/consumer/resource/event/EventService.kt
@@ -3,9 +3,7 @@ package no.fintlabs.consumer.resource.event
 import com.github.benmanes.caffeine.cache.Cache
 import lombok.RequiredArgsConstructor
 import lombok.extern.slf4j.Slf4j
-import no.fint.model.resource.FintResource
 import no.fintlabs.adapter.models.event.ResponseFintEvent
-import no.fintlabs.cache.CacheService
 import org.springframework.stereotype.Service
 
 @Service
@@ -14,7 +12,6 @@ import org.springframework.stereotype.Service
 class EventService(
     private val requestFintCorrIds: Cache<String, String>,
     private val responseFintEvents: Cache<String, ResponseFintEvent>,
-    private val cacheService: CacheService,
 ) {
     fun registerResponse(
         key: String,
@@ -26,23 +23,4 @@ class EventService(
     fun getResponse(corrId: String): ResponseFintEvent? = responseFintEvents.getIfPresent(corrId)
 
     fun requestExists(corrId: String): Boolean = requestFintCorrIds.getIfPresent(corrId) != null
-
-    fun getResource(
-        resourceName: String,
-        corrId: String,
-    ): FintResource? =
-        responseFintEvents
-            .getIfPresent(corrId)
-            ?.takeIf { it.matchesLastDelivered(resourceName) }
-            ?.let { cacheService.getCache(resourceName).get(it.value.identifier) }
-
-    /**
-     * Checks if the existing resource in the cache has already been updated with this event.
-     *
-     * If the existing resource in cache `lastDelivered` matches `handledAt`, it means that
-     * the Event has been fully processed. Since the `lastDelivered` timestamp matches
-     * the event's timestamp, we can assume the cache is consistent with this event version.
-     */
-    private fun ResponseFintEvent.matchesLastDelivered(resourceName: String): Boolean =
-        cacheService.getCache(resourceName).getLastDelivered(value.identifier) == handledAt
 }
