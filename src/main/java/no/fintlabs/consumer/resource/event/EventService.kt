39fcdb0523e867e6e8854b1131f251a36d07d172
Hknots <hknots99@gmail.com>
2025-12-11 20:58:18 +0100

    refactor: remove unused annotations @Slf4j and @RequiredArgsConstructor from EventService

--- src/main/java/no/fintlabs/consumer/resource/event/EventService.kt
+++ src/main/java/no/fintlabs/consumer/resource/event/EventService.kt
@@ -1,14 +1,10 @@
 package no.fintlabs.consumer.resource.event
 
 import com.github.benmanes.caffeine.cache.Cache
-import lombok.RequiredArgsConstructor
-import lombok.extern.slf4j.Slf4j
 import no.fintlabs.adapter.models.event.ResponseFintEvent
 import org.springframework.stereotype.Service
 
 @Service
-@Slf4j
-@RequiredArgsConstructor
 class EventService(
     private val requestFintCorrIds: Cache<String, String>,
     private val responseFintEvents: Cache<String, ResponseFintEvent>,
