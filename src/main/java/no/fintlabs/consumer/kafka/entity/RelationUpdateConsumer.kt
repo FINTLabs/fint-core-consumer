7d88997737355c1ef88679387a64eefcc3d45ced
Hknots <hknots99@gmail.com>
2025-12-12 12:44:23 +0100

    refactor: add ConditionalOnProperty to RelationUpdateConsumer to enable auto-relation configuration

--- src/main/java/no/fintlabs/consumer/kafka/entity/RelationUpdateConsumer.kt
+++ src/main/java/no/fintlabs/consumer/kafka/entity/RelationUpdateConsumer.kt
@@ -6,33 +6,35 @@ import no.fintlabs.consumer.links.relation.RelationService
 import no.fintlabs.kafka.entity.EntityConsumerFactoryService
 import no.fintlabs.kafka.entity.topic.EntityTopicNameParameters
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.stereotype.Component
 
 @Configuration
 open class RelationUpdateConsumer(
     private val relationService: RelationService,
-    private val consumerConfig: ConsumerConfiguration
+    private val consumerConfig: ConsumerConfiguration,
 ) {
-
     @Bean
+    @ConditionalOnProperty(name = ["fint.consumer.autorelation"], havingValue = "true")
     open fun relationUpdateConsumerContainer(consumerFactoryService: EntityConsumerFactoryService) =
         consumerFactoryService
             .createFactory(RelationUpdate::class.java, this::consumeRecord)
             .createContainer(
-                EntityTopicNameParameters.builder()
+                EntityTopicNameParameters
+                    .builder()
                     .orgId("fintlabs-no")
                     .domainContext("fint-core")
                     .resource("relation-update")
-                    .build()
+                    .build(),
             )
 
     fun consumeRecord(consumerRecord: ConsumerRecord<String, RelationUpdate>) =
-        consumerRecord.value().takeIf { belongsToThisService(it) }
+        consumerRecord
+            .value()
+            .takeIf { belongsToThisService(it) }
             ?.let { relationService.processRelationUpdate(it) }
 
     private fun belongsToThisService(relationUpdate: RelationUpdate) =
         consumerConfig.matchesConfiguration(relationUpdate.domainName, relationUpdate.packageName, relationUpdate.orgId)
-
 }
