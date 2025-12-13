8c22ea203de78d619623de30a73070963c49b482
Hknots <hknots99@gmail.com>
2025-12-13 16:41:43 +0100

    refactor: simplify topic creation logic by introducing `createTopics` method and utilizing `resourceContext`

--- src/main/java/no/fintlabs/consumer/kafka/entity/EntityConsumer.kt
+++ src/main/java/no/fintlabs/consumer/kafka/entity/EntityConsumer.kt
@@ -3,13 +3,13 @@ package no.fintlabs.consumer.kafka.entity
 import no.fintlabs.consumer.config.ConsumerConfiguration
 import no.fintlabs.consumer.resource.ResourceMapperService
 import no.fintlabs.consumer.resource.ResourceService
+import no.fintlabs.consumer.resource.context.ResourceContext
 import no.novari.kafka.consuming.ErrorHandlerConfiguration
 import no.novari.kafka.consuming.ErrorHandlerFactory
 import no.novari.kafka.consuming.ListenerConfiguration
 import no.novari.kafka.consuming.ParameterizedListenerContainerFactoryService
-import no.novari.kafka.topic.name.EntityTopicNamePatternParameters
-import no.novari.kafka.topic.name.TopicNamePatternParameterPattern
-import no.novari.kafka.topic.name.TopicNamePatternPrefixParameters
+import no.novari.kafka.topic.name.EntityTopicNameParameters
+import no.novari.kafka.topic.name.TopicNamePrefixParameters
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.context.annotation.Bean
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer
@@ -20,6 +20,7 @@ class EntityConsumer(
     private val resourceService: ResourceService,
     private val consumerConfig: ConsumerConfiguration,
     private val resourceMapper: ResourceMapperService,
+    private val resourceContext: ResourceContext,
 ) {
     @Bean
     fun resourceEntityConsumerFactory(
@@ -44,18 +45,7 @@ class EntityConsumer(
                         .skipFailedRecords() // TODO: We should send to DLQ - but skip temporarily
                         .build(),
                 ),
-            ).createContainer(
-                EntityTopicNamePatternParameters
-                    .builder()
-                    .topicNamePatternPrefixParameters(
-                        TopicNamePatternPrefixParameters
-                            .stepBuilder()
-                            .orgId(TopicNamePatternParameterPattern.exactly(createOrgId()))
-                            .domainContextApplicationDefault()
-                            .build(),
-                    ).resource(TopicNamePatternParameterPattern.startingWith(createResourcePattern()))
-                    .build(),
-            )
+            ).createContainer(createTopics())
 
     fun consumeRecord(consumerRecord: ConsumerRecord<String, Any>) {
         val resourceName = consumerRecord.getResourceName()
@@ -64,11 +54,25 @@ class EntityConsumer(
         resourceService.processEntityConsumerRecord(kafkaEntity)
     }
 
+    private fun createTopics(): Collection<EntityTopicNameParameters> =
+        resourceContext.resourceNames.map { resourceName ->
+            EntityTopicNameParameters
+                .builder()
+                .topicNamePrefixParameters(
+                    TopicNamePrefixParameters
+                        .stepBuilder()
+                        .orgId(createOrgId())
+                        .domainContextApplicationDefault()
+                        .build(),
+                ).resourceName(resourceName.toTopicResource())
+                .build()
+        }
+
+    private fun String.toTopicResource(): String = "${consumerConfig.domain}-${consumerConfig.packageName}-$this".lowercase()
+
     private fun ConsumerRecord<String, Any>.getResourceName() = topic().substringAfterLast("-")
 
     private fun ConsumerRecord<*, Any>.convertResource(resourceName: String) = resourceMapper.mapResource(resourceName, value())
 
     private fun createOrgId() = consumerConfig.orgId.replace(".", "-")
-
-    private fun createResourcePattern() = "${consumerConfig.domain}-${consumerConfig.packageName}"
 }
