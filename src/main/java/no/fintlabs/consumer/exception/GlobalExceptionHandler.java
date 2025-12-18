9da5de69913109f664f7d555d3f209fb8f14f66e
Hknots <hknots99@gmail.com>
2025-12-18 15:31:37 +0100

    test: add integration tests for GlobalExceptionHandler and extend error handling logic

--- src/main/java/no/fintlabs/consumer/exception/GlobalExceptionHandler.java
+++ src/main/java/no/fintlabs/consumer/exception/GlobalExceptionHandler.java
@@ -1,23 +1,27 @@
 package no.fintlabs.consumer.exception;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.consumer.config.ConsumerConfiguration;
 import no.fintlabs.consumer.exception.kafka.ConsumerErrorPublisher;
 import no.fintlabs.status.models.error.ConsumerError;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
 
 
 @ControllerAdvice
 @RequiredArgsConstructor
-public class GlobalExceptionHandler {
+@Slf4j
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     private final ConsumerErrorPublisher consumerErrorPublisher;
     private final ConsumerConfiguration configuration;
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleExceptions(Exception ex) {
+        logger.error("Caught in global exception handler:", ex);
         consumerErrorPublisher.publish(ConsumerError.fromException(
                 ex,
                 configuration.getDomain(),
