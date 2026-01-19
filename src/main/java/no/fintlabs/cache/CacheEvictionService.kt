21cbf0a1a9398bf0bba23ffec2183ec978341641
Hknots <hknots99@gmail.com>
2026-01-19 21:31:46 +0100

    refactor: replace `onCacheEviction` with `relationEventService` for managing cache eviction relations

--- src/main/java/no/fintlabs/cache/CacheEvictionService.kt
+++ src/main/java/no/fintlabs/cache/CacheEvictionService.kt
@@ -1,27 +1,19 @@
 package no.fintlabs.cache
 
-import no.fintlabs.consumer.config.ConsumerConfiguration
+import no.fintlabs.autorelation.RelationEventService
 import org.springframework.stereotype.Service
 
 @Service
 class CacheEvictionService(
     private val cacheService: CacheService,
-    private val consumerConfig: ConsumerConfiguration,
+    private val relationEventService: RelationEventService,
 ) {
     fun evictExpired(resourceName: String) =
         cacheService
             .getCache(resourceName)
             ?.let { cache ->
                 cache.evictOldCacheObjects { key, cacheObject ->
-                    onCacheEviction(key, resourceName, cacheObject.unboxObject())
+                    relationEventService.removeRelations(resourceName, key, cacheObject.unboxObject())
                 }
             }
-
-    private fun onCacheEviction(
-        key: String,
-        resourceName: String,
-        resource: Any,
-    ) {
-        // TODO: If it has managed relations, send relation update
-    }
 }
