80cd68b33af4f10f35aa9ce0eb49184e33a19494
Hknots <hknots99@gmail.com>
2026-01-19 18:48:43 +0100

    feat: add `AutoRelationEntityConsumer` and `AutoRelationService` for handling new entity processing

---
+++ src/main/java/no/fintlabs/autorelation/AutoRelationEntityConsumer.kt
@@ -0,0 +1,46 @@
+package no.fintlabs.autorelation
+
+import no.fintlabs.consumer.config.ConsumerConfiguration
+import no.fintlabs.kafka.common.topic.pattern.FormattedTopicComponentPattern
+import no.fintlabs.kafka.entity.EntityConsumerConfiguration
+import no.fintlabs.kafka.entity.EntityConsumerFactoryService
+import no.fintlabs.kafka.entity.topic.EntityTopicNamePatternParameters
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer
+
+@Configuration
+class AutoRelationEntityConsumer(
+    private val consumerConfig: ConsumerConfiguration,
+    private val autoRelationService: AutoRelationService,
+) {
+    @Bean
+    fun buildAutoRelationConsumer(
+        consumerFactoryService: EntityConsumerFactoryService,
+    ): ConcurrentMessageListenerContainer<String, in Any> =
+        consumerFactoryService
+            .createFactory(
+                Any::class.java,
+                this::consumeRecord,
+                EntityConsumerConfiguration
+                    .builder()
+                    .groupIdSuffix("autorelation")
+                    .seekingOffsetResetOnAssignment(false)
+                    .build(),
+            ).createContainer(
+                EntityTopicNamePatternParameters
+                    .builder()
+                    .orgId(FormattedTopicComponentPattern.anyOf(createOrgId()))
+                    .domainContext(FormattedTopicComponentPattern.anyOf("fint-core"))
+                    .resource(FormattedTopicComponentPattern.startingWith(createResourcePattern()))
+                    .build(),
+            )
+
+    fun consumeRecord(consumerRecord: ConsumerRecord<String, Any>) =
+        autoRelationService.handleNewEntity(consumerRecord.key(), consumerRecord.value())
+
+    private fun createOrgId() = consumerConfig.orgId.replace(".", "-")
+
+    private fun createResourcePattern() = "${consumerConfig.domain}-${consumerConfig.packageName}"
+}
---
+++ src/main/java/no/fintlabs/autorelation/AutoRelationService.kt
@@ -0,0 +1,12 @@
+package no.fintlabs.autorelation
+
+import org.springframework.stereotype.Service
+
+@Service
+class AutoRelationService {
+    fun handleNewEntity(
+        resourceId: String,
+        Resource: Any,
+    ) {
+    }
+}
