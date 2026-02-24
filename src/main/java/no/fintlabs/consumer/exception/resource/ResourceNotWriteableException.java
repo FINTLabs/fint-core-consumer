dfe855c809e1b7cc851a58fe958a1e1d6fc8ffc8
Hknots <hknots99@gmail.com>
2026-02-24 18:49:28 +0100

    refactor!: include resource name in `ResourceNotWriteableException` message and adjust HTTP status in handler

--- src/main/java/no/fintlabs/consumer/exception/resource/ResourceNotWriteableException.java
+++ src/main/java/no/fintlabs/consumer/exception/resource/ResourceNotWriteableException.java
@@ -1,8 +1,13 @@
 package no.fintlabs.consumer.exception.resource;
 
+import lombok.Getter;
+
 public class ResourceNotWriteableException extends RuntimeException {
+    @Getter
+    private final String resourceName;
 
     public ResourceNotWriteableException(String resourceName) {
         super("%s is not writeable, therefore it does not have access to writeable actions.".formatted(resourceName));
+        this.resourceName = resourceName;
     }
 }
