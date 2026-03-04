a1cff3eec9cef0112943c49f2f7b9f4931a0aca3
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2026-03-04 09:50:13 +0100

    feature: run cache eviction asynchronously

--- src/main/java/no/fintlabs/Application.java
+++ src/main/java/no/fintlabs/Application.java
@@ -4,9 +4,11 @@ import no.fint.antlr.EnableFintFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFintFilter
+@EnableAsync
 @EnableScheduling
 @SpringBootApplication
 @ConfigurationPropertiesScan
