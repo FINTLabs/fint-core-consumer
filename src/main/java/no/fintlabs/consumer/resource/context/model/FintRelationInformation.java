7dacacea405b329ff6270e4075322757ee7275e9
Hknots <hknots99@gmail.com>
2026-03-09 08:39:41 +0100

    fix: preserve original casing for packageName

--- src/main/java/no/fintlabs/consumer/resource/context/model/FintRelationInformation.java
+++ src/main/java/no/fintlabs/consumer/resource/context/model/FintRelationInformation.java
@@ -14,7 +14,6 @@ public record FintRelationInformation(
 
     public FintRelationInformation {
         if (!name.equals(name.toLowerCase())) throw new IllegalArgumentException("name must be lowercase: " + name);
-        if (!packageName.equals(packageName.toLowerCase())) throw new IllegalArgumentException("packageName must be lowercase: " + packageName);
         if (!uri.equals(uri.toLowerCase())) throw new IllegalArgumentException("uri must be lowercase: " + uri);
         for (String idField : idFields) {
             if (!idField.equals(idField.toLowerCase())) throw new IllegalArgumentException("idField must be lowercase: " + idField);
@@ -24,7 +23,7 @@ public record FintRelationInformation(
     public static FintRelationInformation byModelObject(String name, String packageName, FintModelObject fintModelObject, String uri) {
         return new FintRelationInformation(
             name.toLowerCase(),
-            packageName.toLowerCase(),
+            packageName,
             fintModelObject.getIdentifikators().keySet().stream()
                 .map(String::toLowerCase)
                 .collect(Collectors.toSet()),
