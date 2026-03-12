b166f83a9e5a73bf56be9ad14dd5acc66ed7cf71
Hknots <hknots99@gmail.com>
2026-03-12 10:31:44 +0100

    refactor: get resource name from header or throw exception

--- src/main/java/no/fintlabs/consumer/kafka/entity/EntityConsumer.kt
+++ src/main/java/no/fintlabs/consumer/kafka/entity/EntityConsumer.kt
@@ -1,9 +1,10 @@
 package no.fintlabs.consumer.kafka.entity
 
 import no.fintlabs.consumer.config.ConsumerConfiguration
+import no.fintlabs.consumer.kafka.KafkaConstants.RESOURCE_NAME
 import no.fintlabs.consumer.kafka.KafkaConsumerErrorHandling
+import no.fintlabs.consumer.kafka.stringValue
 import no.fintlabs.consumer.resource.ResourceConverter
-import no.novari.fint.model.resource.FintResource
 import no.novari.kafka.consuming.ErrorHandlerFactory
 import no.novari.kafka.consuming.ListenerConfiguration
 import no.novari.kafka.consuming.ParameterizedListenerContainerFactoryService
@@ -68,22 +69,16 @@ class EntityConsumer(
             .let { entityProcessingService.processEntityConsumerRecord(it) }
 
     private fun createEntityConsumerRecord(consumerRecord: ConsumerRecord<String, Any?>) =
-        getResourceName(consumerRecord.topic()).let { resourceName ->
-
+        consumerRecord.getResourceName().let { resourceName ->
             consumerRecord
                 .value()
                 ?.let { resourceConverter.convert(resourceName, it) }
-                ?.let { createKafkaEntity(resourceName, it, consumerRecord) }
-                ?: createKafkaEntity(resourceName, null, consumerRecord)
+                ?.let { EntityConsumerRecord(resourceName, it, consumerRecord) }
+                ?: EntityConsumerRecord(resourceName, null, consumerRecord)
         }
-
-    private fun createKafkaEntity(
-        resourceName: String,
-        resource: FintResource?,
-        consumerRecord: ConsumerRecord<String, Any?>,
-    ) = EntityConsumerRecord(resourceName, resource, record = consumerRecord)
 
     private fun createResourcePattern() = "${consumerConfig.domain}-${consumerConfig.packageName}"
 
-    private fun getResourceName(topic: String) = topic.substringAfterLast("-")
+    private fun ConsumerRecord<String, Any?>.getResourceName(): String =
+        headers().stringValue(RESOURCE_NAME) ?: throw IllegalArgumentException("Resource name header not found")
 }
