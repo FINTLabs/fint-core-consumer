100659a84d9f40798af43efd8db4f7306e88330f
Hknots <hknots99@gmail.com>
2026-03-18 19:29:32 +0100

    refactor: rename lastFullSync to lastCompletedFullSync

--- src/main/java/no/fintlabs/consumer/admin/CacheEntry.java
+++ src/main/java/no/fintlabs/consumer/admin/CacheEntry.java
@@ -2,5 +2,5 @@ package no.fintlabs.consumer.admin;
 
 import java.util.Date;
 
-public record CacheEntry(Date lastUpdated, Date lastFullSync, Integer size) {
+public record CacheEntry(Date lastUpdated, Date lastCompletedFullSync, Integer size) {
 }
