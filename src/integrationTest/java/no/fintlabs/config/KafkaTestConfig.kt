744ad2a53f7519f95ded4e2cbd452ccbdfb92f82
Hknots <hknots99@gmail.com>
2026-03-19 23:27:40 +0100

    chore: remove reduntant KafkaTestConfig, as TestComponent is good enough

--- src/integrationTest/java/no/fintlabs/config/KafkaTestConfig.kt
+++
@@ -1,9 +0,0 @@
-package no.fintlabs.config
-
-import no.fintlabs.utils.EntityProducer
-import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.context.annotation.Import
-
-@TestConfiguration
-@Import(EntityProducer::class)
-class KafkaTestConfig
