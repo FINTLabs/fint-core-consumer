1b5c9bd38590463504d9778cacfb94b9c021f99c
Hknots <hknots99@gmail.com>
2026-04-09 18:00:48 +0200

    refactor: simplify `RequestFintEventProducer` by removing topic initialization logic and replacing custom producer with `KafkaTemplate`

--- src/main/java/no/fintlabs/consumer/kafka/event/RequestFintEventProducer.kt
+++ src/main/java/no/fintlabs/consumer/kafka/event/RequestFintEventProducer.kt
@@ -2,6 +2,7 @@ package no.fintlabs.consumer.kafka.event
 
 import no.fintlabs.adapter.models.event.RequestFintEvent
 import no.fintlabs.consumer.config.ConsumerConfiguration
+import no.fintlabs.kafka.KafkaTopicName
 import no.novari.kafka.producing.ParameterizedProducerRecord
 import no.novari.kafka.producing.ParameterizedTemplateFactory
 import no.novari.kafka.topic.EventTopicService
@@ -10,54 +11,20 @@ import no.novari.kafka.topic.configuration.EventTopicConfiguration
 import no.novari.kafka.topic.name.EventTopicNameParameters
 import no.novari.kafka.topic.name.TopicNamePrefixParameters
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 
-@Service
+@Component
 class RequestFintEventProducer(
-    parameterizedTemplateFactory: ParameterizedTemplateFactory,
-    eventTopicService: EventTopicService,
-    private val consumerConfig: ConsumerConfiguration,
+    private val kafkaTemplate: KafkaTemplate<String, RequestFintEvent>,
+    private val eventResponseTopicPattern: String,
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
-    private val producer = parameterizedTemplateFactory.createTemplate(RequestFintEvent::class.java)
-
-    private val topicNameParameters =
-        EventTopicNameParameters
-            .builder()
-            .topicNamePrefixParameters(
-                TopicNamePrefixParameters
-                    .stepBuilder()
-                    .orgId(consumerConfig.orgId.asTopicSegment)
-                    .domainContextApplicationDefault()
-                    .build(),
-            ).eventName("${consumerConfig.domain}-${consumerConfig.packageName}-request")
-            .build()
-
-    init {
-        ensureTopicExists(eventTopicService)
-    }
 
     fun produce(requestFintEvent: RequestFintEvent) {
-        logger.info("Publishing RequestFintEvent: {}", requestFintEvent.corrId)
-        producer.send(
-            ParameterizedProducerRecord
-                .builder<RequestFintEvent>()
-                .key(requestFintEvent.corrId)
-                .topicNameParameters(topicNameParameters)
-                .value(requestFintEvent)
-                .build(),
-        )
-    }
-
-    private fun ensureTopicExists(eventTopicService: EventTopicService) {
-        eventTopicService.createOrModifyTopic(
-            topicNameParameters,
-            EventTopicConfiguration
-                .stepBuilder()
-                .partitions(consumerConfig.kafka.requestPartitions)
-                .retentionTime(consumerConfig.kafka.requestRetentionTime)
-                .cleanupFrequency(EventCleanupFrequency.NORMAL)
-                .build(),
-        )
+        logger.info("Producing RequestFintEvent: {}", requestFintEvent.corrId)
+        kafkaTemplate.send(eventResponseTopicPattern, requestFintEvent.corrId, requestFintEvent)
     }
 }
