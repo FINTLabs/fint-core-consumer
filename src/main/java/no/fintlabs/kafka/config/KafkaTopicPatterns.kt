136748529fe87e3e07f68888fb349adffdbe63cd
Hknots <hknots99@gmail.com>
2026-04-09 17:35:21 +0200

    refactor: extract `component` property and add `eventRequest`/`eventResponse` topic patterns in `KafkaTopicPatterns` for improved clarity and configurability

--- src/main/java/no/fintlabs/kafka/config/KafkaTopicPatterns.kt
+++ src/main/java/no/fintlabs/kafka/config/KafkaTopicPatterns.kt
@@ -9,17 +9,34 @@ import org.springframework.context.annotation.Configuration
 class KafkaTopicPatterns(
     private val consumerConfig: ConsumerConfiguration,
 ) {
+    private val component: String
+        get() = "${consumerConfig.domain}-${consumerConfig.packageName}"
+
     @Bean
     fun resourceTopicPattern(): String =
         KafkaTopicName.entity(
             consumerConfig.orgId,
-            "${consumerConfig.domain}-${consumerConfig.packageName}",
+            component,
         )
 
     @Bean
     fun relationUpdateTopicPattern(): String =
         KafkaTopicName.entity(
             consumerConfig.orgId,
-            "${consumerConfig.domain}-${consumerConfig.packageName}-relation-update",
+            "$component-relation-update",
+        )
+
+    @Bean
+    fun eventRequestTopicPattern(): String =
+        KafkaTopicName.event(
+            consumerConfig.orgId,
+            "$component-request",
+        )
+
+    @Bean
+    fun eventResponseTopicPattern(): String =
+        KafkaTopicName.event(
+            consumerConfig.orgId,
+            "$component-response",
         )
 }
