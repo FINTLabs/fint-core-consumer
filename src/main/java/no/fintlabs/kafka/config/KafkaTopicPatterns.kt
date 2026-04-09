89b4d646a61c61519e7f1443a72a20289b77221a
Hknots <hknots99@gmail.com>
2026-04-09 13:40:56 +0200

    chore: add `KafkaTopicPatterns` for defining dynamic entity topic patterns

---
+++ src/main/java/no/fintlabs/kafka/config/KafkaTopicPatterns.kt
@@ -0,0 +1,15 @@
+package no.fintlabs.kafka.config
+
+import no.fintlabs.consumer.config.ConsumerConfiguration
+import no.fintlabs.kafka.KafkaTopicNameConstants.ENTITY
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class KafkaTopicPatterns(
+    private val consumerConfig: ConsumerConfiguration,
+) {
+    @Bean
+    fun entityTopicPattern(): String =
+        "${consumerConfig.orgId.asTopicSegment}.fint-core.$ENTITY.${consumerConfig.domain}-${consumerConfig.packageName}"
+}
