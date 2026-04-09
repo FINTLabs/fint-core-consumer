48bdbd91613b26b4422bdecedafb5f7d2d908b53
Hknots <hknots99@gmail.com>
2026-04-09 13:41:02 +0200

    chore: add `KafkaConsumerConfig` for configuring Kafka consumer listener factories

---
+++ src/main/java/no/fintlabs/kafka/config/KafkaConsumerConfig.kt
@@ -0,0 +1,30 @@
+package no.fintlabs.kafka.config
+
+import no.fintlabs.kafka.KafkaConsumerNames
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+
+@Configuration
+class KafkaConsumerConfig(
+    private val kafkaProperties: KafkaProperties,
+    private val consumerFactory: ConsumerFactory<String, Any>,
+) {
+    @Bean
+    fun entityFactory(): ConcurrentKafkaListenerContainerFactory<String, Any> =
+        createFactory(kafkaProperties.consumers[KafkaConsumerNames.ENTITY])
+
+    @Bean
+    fun autoRelationEntityFactory(): ConcurrentKafkaListenerContainerFactory<String, Any> =
+        createFactory(kafkaProperties.consumers[KafkaConsumerNames.AUTO_RELATION_ENTITY])
+
+    private fun createFactory(
+        props: KafkaProperties.ConsumerProperties?,
+    ): ConcurrentKafkaListenerContainerFactory<String, Any> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, Any>()
+        factory.consumerFactory = consumerFactory
+        factory.setConcurrency(props?.concurrency ?: 1)
+        return factory
+    }
+}
