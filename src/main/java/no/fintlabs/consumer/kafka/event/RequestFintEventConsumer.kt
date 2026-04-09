5247e0b28d563df9c056e7fd7ed711df5c2a3851
Hknots <hknots99@gmail.com>
2026-04-09 18:05:23 +0200

    chore: remove unused imports from `RequestFintEventConsumer` to clean up dependencies

--- src/main/java/no/fintlabs/consumer/kafka/event/RequestFintEventConsumer.kt
+++ src/main/java/no/fintlabs/consumer/kafka/event/RequestFintEventConsumer.kt
@@ -1,24 +1,13 @@
 package no.fintlabs.consumer.kafka.event
 
 import no.fintlabs.adapter.models.event.RequestFintEvent
-import no.fintlabs.consumer.config.ConsumerConfiguration
-import no.fintlabs.consumer.kafka.KafkaConsumerErrorHandling
 import no.fintlabs.consumer.resource.event.EventStatusCache
 import no.fintlabs.kafka.KafkaConsumerNames.EVENT
-import no.fintlabs.kafka.KafkaConsumerNames.RESOURCE
 import no.fintlabs.kafka.config.ConfigurableConsumer
 import no.fintlabs.kafka.config.KafkaProperties
-import no.novari.kafka.consuming.ErrorHandlerFactory
-import no.novari.kafka.consuming.ListenerConfiguration
-import no.novari.kafka.consuming.ParameterizedListenerContainerFactoryService
-import no.novari.kafka.topic.name.EventTopicNameParameters
-import no.novari.kafka.topic.name.TopicNamePrefixParameters
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.slf4j.LoggerFactory
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer
 
 @Configuration
 class RequestFintEventConsumer(
