35ea88d5ee42256d2a0cc6de6d92c129eb470c18
Hknots <hknots99@gmail.com>
2026-04-09 18:03:00 +0200

    chore: remove unused imports from `RequestFintEventProducer` to clean up dependencies

--- src/main/java/no/fintlabs/consumer/kafka/event/RequestFintEventProducer.kt
+++ src/main/java/no/fintlabs/consumer/kafka/event/RequestFintEventProducer.kt
@@ -1,20 +1,9 @@
 package no.fintlabs.consumer.kafka.event
 
 import no.fintlabs.adapter.models.event.RequestFintEvent
-import no.fintlabs.consumer.config.ConsumerConfiguration
-import no.fintlabs.kafka.KafkaTopicName
-import no.novari.kafka.producing.ParameterizedProducerRecord
-import no.novari.kafka.producing.ParameterizedTemplateFactory
-import no.novari.kafka.topic.EventTopicService
-import no.novari.kafka.topic.configuration.EventCleanupFrequency
-import no.novari.kafka.topic.configuration.EventTopicConfiguration
-import no.novari.kafka.topic.name.EventTopicNameParameters
-import no.novari.kafka.topic.name.TopicNamePrefixParameters
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
-import org.springframework.stereotype.Service
 
 @Component
 class RequestFintEventProducer(
