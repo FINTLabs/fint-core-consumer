5043edf3ad91db18164f5f76419b4fa27777bede
Hknots <hknots99@gmail.com>
2026-04-09 17:17:26 +0200

    feat: add `RELATION_UPDATE` constant to `KafkaConsumerNames`

--- src/main/java/no/fintlabs/kafka/KafkaConsumerNames.kt
+++ src/main/java/no/fintlabs/kafka/KafkaConsumerNames.kt
@@ -3,4 +3,5 @@ package no.fintlabs.kafka
 object KafkaConsumerNames {
     const val RESOURCE = "resource"
     const val AUTO_RELATION_RESOURCE = "auto-relation-resource"
+    const val RELATION_UPDATE = "relation-update"
 }
