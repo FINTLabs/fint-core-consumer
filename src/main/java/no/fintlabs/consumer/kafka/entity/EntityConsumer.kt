6570074c2b6ce7679ade70467166dafb76343ce5
Hknots <hknots99@gmail.com>
2026-04-09 13:41:19 +0200

    refactor: simplify `EntityConsumer` by using `ConfigurableConsumer` and `@KafkaListener`, removing custom factory logic

--- src/main/java/no/fintlabs/consumer/kafka/entity/EntityConsumer.kt
+++ src/main/java/no/fintlabs/consumer/kafka/entity/EntityConsumer.kt
@@ -1,76 +1,28 @@
 package no.fintlabs.consumer.kafka.entity
 
-import no.fintlabs.consumer.config.ConsumerConfiguration
 import no.fintlabs.consumer.kafka.KafkaConstants.RESOURCE_NAME
-import no.fintlabs.consumer.kafka.KafkaConsumerErrorHandling
 import no.fintlabs.consumer.kafka.stringValue
 import no.fintlabs.consumer.resource.ResourceConverter
-import no.novari.kafka.consuming.ErrorHandlerFactory
-import no.novari.kafka.consuming.ListenerConfiguration
-import no.novari.kafka.consuming.ParameterizedListenerContainerFactoryService
-import no.novari.kafka.topic.name.EntityTopicNameParameters
-import no.novari.kafka.topic.name.EntityTopicNamePatternParameters
-import no.novari.kafka.topic.name.TopicNamePatternParameterPattern
-import no.novari.kafka.topic.name.TopicNamePatternParameters
-import no.novari.kafka.topic.name.TopicNamePatternPrefixParameters
-import no.novari.kafka.topic.name.TopicNamePrefixParameters
-import no.novari.metamodel.MetamodelService
+import no.fintlabs.kafka.KafkaConsumerNames.ENTITY
+import no.fintlabs.kafka.config.ConfigurableConsumer
+import no.fintlabs.kafka.config.KafkaProperties
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.slf4j.LoggerFactory
-import org.springframework.context.annotation.Bean
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer
+import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 
 @Service
 class EntityConsumer(
     private val entityProcessingService: EntityProcessingService,
-    private val consumerConfig: ConsumerConfiguration,
     private val resourceConverter: ResourceConverter,
-    private val metamodelService: MetamodelService,
-) {
-    companion object {
-        private val logger = LoggerFactory.getLogger(EntityConsumer::class.java)
-        private const val CONSUMER_NAME = "entity"
+    kafkaProperties: KafkaProperties,
+) : ConfigurableConsumer(kafkaProperties, ENTITY) {
+    @KafkaListener(
+        topicPattern = "\${fint.consumer.domain}.fint-core.entity.\${fint.consumer.domain}-\${fint.consumer.package}",
+    )
+    fun consumeRecord(consumerRecord: ConsumerRecord<String, Any?>) {
+        val entityConsumerRecord = createEntityConsumerRecord(consumerRecord)
+        entityProcessingService.processEntityConsumerRecord(entityConsumerRecord)
     }
-
-    @Bean
-    fun resourceEntityConsumerFactory(
-        parameterizedListenerContainerFactoryService: ParameterizedListenerContainerFactoryService,
-        errorHandlerFactory: ErrorHandlerFactory,
-    ): ConcurrentMessageListenerContainer<String, in Any> =
-        parameterizedListenerContainerFactoryService
-            .createRecordListenerContainerFactory(
-                Any::class.java,
-                this::consumeRecord,
-                ListenerConfiguration
-                    .stepBuilder()
-                    .groupIdApplicationDefault()
-                    .maxPollRecordsKafkaDefault()
-                    .maxPollIntervalKafkaDefault()
-                    .seekToBeginningOnAssignment()
-                    .build(),
-                errorHandlerFactory.createErrorHandler(
-                    KafkaConsumerErrorHandling.createLoggingErrorHandlerConfiguration<Any>(
-                        logger,
-                        CONSUMER_NAME,
-                    ),
-                ),
-            ).createContainer(
-                EntityTopicNamePatternParameters
-                    .builder()
-                    .topicNamePatternPrefixParameters(
-                        TopicNamePatternPrefixParameters
-                            .stepBuilder()
-                            .orgId(TopicNamePatternParameterPattern.exactly(consumerConfig.orgId.asTopicSegment))
-                            .domainContextApplicationDefault()
-                            .build(),
-                    ).resource(TopicNamePatternParameterPattern.anyOf(componentTopic(), *legacyResourceTopics()))
-                    .build(),
-            ).apply { concurrency = consumerConfig.kafka.entityConcurrency }
-
-    fun consumeRecord(consumerRecord: ConsumerRecord<String, Any?>) =
-        createEntityConsumerRecord(consumerRecord)
-            .let { entityProcessingService.processEntityConsumerRecord(it) }
 
     private fun createEntityConsumerRecord(consumerRecord: ConsumerRecord<String, Any?>) =
         consumerRecord.getResourceName().let { resourceName ->
@@ -82,20 +34,5 @@ class EntityConsumer(
         }
 
     private fun ConsumerRecord<String, Any?>.getResourceName(): String =
-        if (consumerConfig.kafka.consumeLegacyResourceTopics) {
-            headers().stringValue(RESOURCE_NAME) ?: topic().split("-").last()
-        } else {
-            headers().stringValue(RESOURCE_NAME) ?: throw IllegalArgumentException("Resource name header not found")
-        }
-
-    private fun componentTopic() = "${consumerConfig.domain}-${consumerConfig.packageName}"
-
-    private fun legacyResourceTopics(): Array<String> {
-        if (!consumerConfig.kafka.consumeLegacyResourceTopics) return emptyArray()
-        return metamodelService
-            .getComponent(consumerConfig.domain, consumerConfig.packageName)!!
-            .resources
-            .map { resource -> "${consumerConfig.domain}-${consumerConfig.packageName}-${resource.name}" }
-            .toTypedArray()
-    }
+        headers().stringValue(RESOURCE_NAME) ?: throw IllegalArgumentException("Resource name header not found")
 }
