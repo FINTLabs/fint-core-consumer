a9dce8ecfa604d13596e2c47c4672ee3aba02b1d
sindre <sindre.giske@novari.no>
2026-04-14 11:54:06 +0200

    Latest Completed FullSync gets registered and cached. cache updates for each finished fullSync. Gauge registered for each resource.

--- src/main/java/no/fintlabs/consumer/kafka/sync/SyncTrackerService.kt
+++ src/main/java/no/fintlabs/consumer/kafka/sync/SyncTrackerService.kt
@@ -16,6 +16,7 @@ import no.fintlabs.consumer.kafka.sync.SyncState.Failed
 import no.fintlabs.consumer.kafka.sync.SyncState.Init
 import no.fintlabs.consumer.kafka.sync.SyncState.ResourceNameChanged
 import no.fintlabs.consumer.kafka.sync.SyncState.TotalSizeChanged
+import no.fintlabs.consumer.kafka.sync.model.LastCompletedFullSync
 import no.fintlabs.consumer.kafka.sync.model.SyncStatus
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -41,6 +42,7 @@ class SyncTrackerService(
     private val syncStatusProducer: SyncStatusProducer,
     private val meterRegistry: MeterRegistry,
     caffeineCacheProperties: CaffeineCacheProperties,
+    private val lastCompletedRegister: LastCompletedFullSync
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
     private val resourceLocks = Striped.lazyWeakLock(32)
@@ -166,6 +168,7 @@ class SyncTrackerService(
                 timed(resourceName, syncType, "sync.status.publish.completed") {
                     syncStatusProducer.publish(SyncStatus(correlationId, newSyncState.syncType, "Completed"))
                 }
+                lastCompletedRegister.registerTimestamp(resourceName, newSyncState.timestamp)
             }
         } else {
             timed(resourceName, syncType, "sync.state.store") {
